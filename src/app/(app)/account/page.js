27d9c937b5fb567f67f31b83dc4c4a9dc8d4039b
retrovisor <andrenfaust@gmail.com>
2024-05-14 16:57:27 +0200

    Update page.js

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -25,10 +25,7 @@ export default async function AccountPage({ searchParams }) {
 
   // Ensure mongoose connection is established only once
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+    await mongoose.connect(process.env.MONGO_URI);
   }
 
   try {
