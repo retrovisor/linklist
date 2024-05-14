4bdebc7ffe761ecaa03230912fdb079731031dc2
retrovisor <andrenfaust@gmail.com>
2024-05-14 16:46:49 +0200

    Update page.js

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -4,37 +4,28 @@ import PageLinksForm from "@/components/forms/PageLinksForm";
 import PageSettingsForm from "@/components/forms/PageSettingsForm";
 import UsernameForm from "@/components/forms/UsernameForm";
 import { Page } from "@/models/Page";
+import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import cloneDeep from 'clone-deep';
-import clientPromise from '@/libs/mongoClient';
 
 export default async function AccountPage({ searchParams }) {
-  console.log('AccountPage function started');
-
   const session = await getServerSession(authOptions);
-  console.log('Session:', session);
-
   const desiredUsername = searchParams?.desiredUsername;
-  console.log('Desired Username:', desiredUsername);
 
   if (!session) {
-    console.log('No session, redirecting to /');
     return redirect('/');
   }
 
+  // Ensure mongoose connection is established only once
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(process.env.MONGO_URI);
+  }
+
   try {
-    const client = await clientPromise;
-    const db = client.db();
-
-    console.log('Connected to MongoDB');
-
-    const page = await db.collection('pages').findOne({ owner: session?.user?.email });
-    console.log('Query executed successfully');
-    console.log('Page:', page);
+    const page = await Page.findOne({ owner: session?.user?.email });
 
     if (!page) {
-      console.log('Page not found for the user');
       return (
         <div>
           <UsernameForm desiredUsername={desiredUsername} />
@@ -42,12 +33,8 @@ export default async function AccountPage({ searchParams }) {
       );
     }
 
-    console.log('Page found:', page);
-
-    const leanPage = cloneDeep(page);
+    const leanPage = cloneDeep(page.toJSON());
     leanPage._id = leanPage._id.toString();
-
-    console.log('Rendering page components');
 
     return (
       <>
