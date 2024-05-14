0afa4f209ce9db5de0983b9ac8df48d9a3301b02
retrovisor <andrenfaust@gmail.com>
2024-05-14 16:54:25 +0200

    Update page.js

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -10,22 +10,34 @@ import { redirect } from "next/navigation";
 import cloneDeep from 'clone-deep';
 
 export default async function AccountPage({ searchParams }) {
+  console.log('AccountPage function started');
+
   const session = await getServerSession(authOptions);
+  console.log('Session:', session);
+
   const desiredUsername = searchParams?.desiredUsername;
+  console.log('Desired Username:', desiredUsername);
 
   if (!session) {
+    console.log('No session, redirecting to /');
     return redirect('/');
   }
 
   // Ensure mongoose connection is established only once
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
   }
 
   try {
     const page = await Page.findOne({ owner: session?.user?.email });
+    console.log('Query executed successfully');
+    console.log('Page:', page);
 
     if (!page) {
+      console.log('Page not found for the user');
       return (
         <div>
           <UsernameForm desiredUsername={desiredUsername} />
@@ -33,8 +45,11 @@ export default async function AccountPage({ searchParams }) {
       );
     }
 
+    console.log('Page found:', page);
+
     const leanPage = cloneDeep(page.toJSON());
     leanPage._id = leanPage._id.toString();
+    console.log('Lean Page:', leanPage);
 
     return (
       <>
