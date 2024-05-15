55e805620269d35afb758a35ee7dc42f1579d572
retrovisor <andrenfaust@gmail.com>
2024-05-15 18:24:56 +0200

    Update LoginWithKakao.js

--- src/components/buttons/LoginWithKakao.js
+++ src/components/buttons/LoginWithKakao.js
@@ -1,14 +1,14 @@
 'use client';
 import { signIn } from "next-auth/react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faKakao } from "@fortawesome/free-brands-svg-icons"; // Assuming you have a Kakao icon in your library
+import Image from "next/image";
+import kakaoLogo from "@/public/kakao.png"; // Ensure you have the Kakao logo in your public folder
 
 export default function LoginWithKakao() {
   return (
     <button
       onClick={() => signIn('kakao')}
       className="bg-yellow-400 shadow text-center w-full py-4 flex gap-3 items-center justify-center">
-      <FontAwesomeIcon icon={faKakao} className="h-6" />
+      <Image src={kakaoLogo} alt="Kakao" width={24} height={24} className="h-6" />
       <span>Sign In with Kakao</span>
     </button>
   );
