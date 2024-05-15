176005abc1fce856922bcaee17bac94e46a78381
retrovisor <andrenfaust@gmail.com>
2024-05-15 19:59:24 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ import Link from "next/link";
 export default async function Header() {
   const session = await getServerSession(authOptions);
   return (
-    <header className="bg-white border-b py-4">
+    <header className="bg-white border-b py-2">
       <div className="max-w-4xl flex justify-between mx-auto px-4">
         <div className="flex items-center gap-6">
           <Link href={'/'} className="flex items-center gap-1 text-blue-500">
@@ -18,8 +18,7 @@ export default async function Header() {
           </Link>
           <nav className="flex items-center gap-4 text-slate-500 text-sm">
             <Link href={'/about'}>About</Link>
-             <Link href={'/contact'}>Contact</Link>
-          </nav>
+           </nav>
         </div>
         <nav className="flex items-center gap-4 text-sm text-slate-500">
           {!!session && (
