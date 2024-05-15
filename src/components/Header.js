7f90c6082cdd743197a2c531db12a767696d3f02
retrovisor <andrenfaust@gmail.com>
2024-05-15 19:56:35 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,16 +10,15 @@ export default async function Header() {
   const session = await getServerSession(authOptions);
   return (
     <header className="bg-white border-b py-4">
-      <div className="max-w-4xl flex justify-between mx-auto px-6">
+      <div className="max-w-4xl flex justify-between mx-auto px-4">
         <div className="flex items-center gap-6">
-          <Link href={'/'} className="flex items-center gap-2 text-blue-500">
+          <Link href={'/'} className="flex items-center gap-1 text-blue-500">
             <FontAwesomeIcon icon={faLink} className="text-blue-500" />
-            <span className="font-bold">LinkList</span>
+            <span className="font-bold">Nae.Link</span>
           </Link>
           <nav className="flex items-center gap-4 text-slate-500 text-sm">
             <Link href={'/about'}>About</Link>
-            <Link href={'/pricing'}>Pricing</Link>
-            <Link href={'/contact'}>Contact</Link>
+             <Link href={'/contact'}>Contact</Link>
           </nav>
         </div>
         <nav className="flex items-center gap-4 text-sm text-slate-500">
