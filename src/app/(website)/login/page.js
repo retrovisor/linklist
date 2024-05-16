77bc3a1bf2beffef9cde38afcff92bc151f5005c
retrovisor <andrenfaust@gmail.com>
2024-05-16 16:33:19 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,39 +1,22 @@
-'use client';
-import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 
 export default function LoginPage() {
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status === "loading") {
-    return <div>Loading...</div>;
-  }
-
-  if (status === "unauthenticated") {
-    return (
-      <div>
-        <div className="p-4 max-w-xs mx-auto">
-          <h1 className="text-4xl font-bold text-center mb-2">
-            Sign In
-          </h1>
-          <p className="text-center mb-6 text-gray-500">
-            Sign in to your account using one of the methods below
-          </p>
-          <LoginWithKakao />
-          <div className="mt-4">
-            <LoginWithGoogle />
-          </div>
+  return (
+    <div>
+      <div className="p-4 max-w-xs mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-2">
+          Sign In
+        </h1>
+        <p className="text-center mb-6 text-gray-500">
+          Sign in to your account using one of the methods below
+        </p>
+        <LoginWithKakao />
+        <div className="mt-4">
+          
+    <LoginWithGoogle />
         </div>
       </div>
-    );
-  }
-
-  const username = localStorage.getItem('desiredUsername');
-  const redirectUrl = username ? `/account?desiredUsername=${username}` : '/account';
-  router.push(redirectUrl);
-
-  return null;
+    </div>
+  );
 }
