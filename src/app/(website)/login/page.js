f939bd7fb4d5ffe8ef62701bc5b379450ad45765
retrovisor <andrenfaust@gmail.com>
2024-05-16 16:02:51 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,7 +1,27 @@
+'use client';
+
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 
 export default function LoginPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      const username = localStorage.getItem('desiredUsername');
+      const redirectUrl = username ? `/account?desiredUsername=${username}` : '/account';
+      router.push(redirectUrl);
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>; // You can replace this with a spinner or loading animation
+  }
+
   return (
     <div>
       <div className="p-4 max-w-xs mx-auto">
@@ -13,8 +33,7 @@ export default function LoginPage() {
         </p>
         <LoginWithKakao />
         <div className="mt-4">
-          
-    <LoginWithGoogle />
+          <LoginWithGoogle />
         </div>
       </div>
     </div>
