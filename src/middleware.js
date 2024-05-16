9afc1305899b25d60dc20ae88f3e4612cbe0cbf1
retrovisor <andrenfaust@gmail.com>
2024-05-16 23:59:47 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,16 @@
-// src/middleware.js
-
+// middleware.js
 import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   console.log('Middleware executed');
-  const token = await getToken({ req, secret: process.env.SECRET });
+  console.log('NEXTAUTH_SECRET:', process.env.NEXTAUTH_SECRET);
+
+  // Log headers and cookies for debugging
+  console.log('Request Headers:', req.headers);
+  console.log('Request Cookies:', req.cookies);
+
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   console.log('Token:', token);
 
   const { pathname } = req.nextUrl;
