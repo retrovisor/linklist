f01bcc6379a830f1ec76805cb32042a767741a49
retrovisor <andrenfaust@gmail.com>
2024-05-16 16:44:13 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const token = await getToken({ req, secret: process.env.SECRET })
 
   const { pathname } = req.nextUrl
 
