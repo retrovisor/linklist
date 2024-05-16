678a0b3675c983daa7093f21e0684032d164aa18
retrovisor <andrenfaust@gmail.com>
2024-05-16 16:29:56 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,41 +1,35 @@
-'use client';
-
-import { useEffect } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
-import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
-import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 
 export default function LoginPage() {
-  const { data: session = null, status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (status === "authenticated") {
-      const username = localStorage.getItem('desiredUsername');
-      const redirectUrl = username ? `/account?desiredUsername=${username}` : '/account';
-      router.push(redirectUrl);
-    }
-  }, [status, router]);
+  const { data: session, status } = useSession();
 
   if (status === "loading") {
-    return <div>Loading...</div>; // You can replace this with a spinner or loading animation
+    return <div>Loading...</div>;
   }
 
-  return (
-    <div>
-      <div className="p-4 max-w-xs mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-2">
-          Sign In
-        </h1>
-        <p className="text-center mb-6 text-gray-500">
-          Sign in to your account using one of the methods below
-        </p>
-        <LoginWithKakao />
-        <div className="mt-4">
-          <LoginWithGoogle />
+  if (status === "unauthenticated") {
+    return (
+      <div>
+        <div className="p-4 max-w-xs mx-auto">
+          <h1 className="text-4xl font-bold text-center mb-2">
+            Sign In
+          </h1>
+          <p className="text-center mb-6 text-gray-500">
+            Sign in to your account using one of the methods below
+          </p>
+          <LoginWithKakao />
+          <div className="mt-4">
+            <LoginWithGoogle />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  // User is authenticated, handle the authenticated state
+  const username = localStorage.getItem('desiredUsername');
+  const redirectUrl = username ? `/account?desiredUsername=${username}` : '/account';
+  router.push(redirectUrl);
+
+  return null;
 }
