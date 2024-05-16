b590ac50cdddae1de193152d7a650dc555709e08
retrovisor <andrenfaust@gmail.com>
2024-05-16 16:51:49 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,24 +1,23 @@
-// middleware.js
+// src/middleware.js
 
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   console.log('Middleware executed');
-  const token = await getToken({ req, secret: process.env.SECRET })
+  const token = await getToken({ req, secret: process.env.SECRET });
   console.log('Token:', token);
 
-  const { pathname } = req.nextUrl
+  const { pathname } = req.nextUrl;
 
   if (token && pathname === '/login') {
     console.log('Redirecting to /account');
-    return NextResponse.redirect(new URL('/account', req.url))
+    return NextResponse.redirect(new URL('/account', req.url));
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
-
 
 export const config = {
-  matcher: '/login'
-}
+  matcher: ['/login'],
+};
