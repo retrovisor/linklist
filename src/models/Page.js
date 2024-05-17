56a4b97ed1a902e9d9c4db74a74dba55ca7ed100
retrovisor <andrenfaust@gmail.com>
2024-05-17 18:36:36 +0200

    Update Page.js

--- src/models/Page.js
+++ src/models/Page.js
@@ -11,6 +11,7 @@ const PageSchema = new Schema({
   bgImage: {type: String, default: ''},
   buttons: {type: Object, default: {}},
   links: {type: Object, default: []},
+  template: {type: String, default: ''}, // Add the new field here
 }, {timestamps: true});
 
 export const Page = models?.Page || model('Page', PageSchema);
