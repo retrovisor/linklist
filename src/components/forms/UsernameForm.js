a8fa70409b26c23b76e269bd30a40043cd227b62
retrovisor <andrenfaust@gmail.com>
2024-05-17 19:18:05 +0200

    Update UsernameForm.js

--- src/components/forms/UsernameForm.js
+++ src/components/forms/UsernameForm.js
@@ -1,22 +1,28 @@
 'use client';
+
 import grabUsername from "@/actions/grabUsername";
 import SubmitButton from "@/components/buttons/SubmitButton";
 import RightIcon from "@/components/icons/RightIcon";
-import {redirect} from "next/navigation";
-import {useState} from "react";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
-export default function UsernameForm({desiredUsername}) {
-  const [taken,setTaken] = useState(false);
-  async function handleSubmit(formData) {
+export default function UsernameForm({ desiredUsername }) {
+  const [taken, setTaken] = useState(false);
+  const router = useRouter();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
     const result = await grabUsername(formData);
 
     setTaken(result === false);
     if (result) {
-      redirect('/account?created='+formData.get('username'));
+      router.push('/select-template?created=' + formData.get('username'));
     }
   }
+
   return (
-    <form action={handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <h1 className="text-4xl font-bold text-center mb-2">
         Grab your username
       </h1>
@@ -29,7 +35,8 @@ export default function UsernameForm({desiredUsername}) {
           className="block p-2 mx-auto border w-full mb-2 text-center"
           defaultValue={desiredUsername}
           type="text"
-          placeholder="username" />
+          placeholder="username"
+        />
         {taken && (
           <div className="bg-red-200 border border-red-500 p-2 mb-2 text-center">
             This username is taken
