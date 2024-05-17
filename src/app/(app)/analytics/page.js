97f56a6f82b6245e9c3ac3793cfe2695ea7238a1
retrovisor <andrenfaust@gmail.com>
2024-05-17 13:16:15 +0200

    Update page.js

--- src/app/(app)/analytics/page.js
+++ src/app/(app)/analytics/page.js
@@ -116,7 +116,7 @@ export default async function AnalyticsPage() {
         <h2 className="text-xl mb-6 text-center">Clicks</h2>
         {page.links && page.links.length ? (
           page.links.map(link => (
-            <div key={link.title} className="md:flex gap-4 items-center border-t border-gray-200 py-4">
+            <div key={link.title || link.url} className="md:flex gap-4 items-center border-t border-gray-200 py-4">
               <div className="text-blue-500 pl-4">
                 <FontAwesomeIcon icon={faLink} />
               </div>
