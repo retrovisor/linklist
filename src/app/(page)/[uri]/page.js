452e5cd3f1adc1a86985467708f4ea2b9bc8bf78
retrovisor <andrenfaust@gmail.com>
2024-05-19 18:35:46 +0200

    Update page.js

--- src/app/(page)/[uri]/page.js
+++ src/app/(page)/[uri]/page.js
@@ -166,7 +166,7 @@ export default async function UserPage({ params }) {
       </div>
 
  <footer className="text-center text-white p-8">
-              <div className="logo-container">
+              <div className="logo-container2">
         <svg class="logo2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 396 97" fill="none">
 
 
