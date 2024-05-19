755e3a5ee107a9b58cc2d49f4856216d46dc0888
retrovisor <andrenfaust@gmail.com>
2024-05-19 15:53:10 +0200

    Update Page.js

--- src/models/Page.js
+++ src/models/Page.js
@@ -1,17 +1,24 @@
-import {model, models, Schema} from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 const PageSchema = new Schema({
-  uri: {type: String, required: true, min: 1, unique: true},
-  owner: {type: String, required: true},
-  displayName: {type: String, default: ''},
-  location: {type: String, default: ''},
-  bio: {type: String, default: ''},
-  bgType: {type: String, default: 'color'},
-  bgColor: {type: String, default: '#000'},
-  bgImage: {type: String, default: ''},
-  buttons: {type: Object, default: {}},
-  links: {type: Object, default: []},
-  template: {type: String, default: ''}, // Add the new field here
-}, {timestamps: true});
+  uri: { type: String, required: true, min: 1, unique: true },
+  owner: { type: String, required: true },
+  displayName: { type: String, default: '' },
+  location: { type: String, default: '' },
+  bio: { type: String, default: '' },
+  bgType: { type: String, default: 'color' },
+  bgColor: { type: String, default: '#000' },
+  bgImage: { type: String, default: '' },
+  buttons: { type: Object, default: {} },
+  links: { type: Array, default: [] },
+  template: { type: String, default: '' },
+  textBoxes: [
+    {
+      key: String,
+      title: String,
+      text: String,
+    },
+  ],
+}, { timestamps: true });
 
 export const Page = models?.Page || model('Page', PageSchema);
