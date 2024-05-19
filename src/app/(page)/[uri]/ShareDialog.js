5ad6b31754830c58e690a064d6f7cd3f80b84b82
retrovisor <andrenfaust@gmail.com>
2024-05-19 18:20:39 +0200

    Update ShareDialog.js

--- src/app/(page)/[uri]/ShareDialog.js
+++ src/app/(page)/[uri]/ShareDialog.js
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShare } from "@fortawesome/free-solid-svg-icons";
 import { faWhatsapp, faFacebook } from "@fortawesome/free-brands-svg-icons"; // Correct import for brands icons
 import { useState, useEffect } from "react";
+import kakaoLogo from '/public/kakao.png'; // Ensure this path is correct
+
 
 // Declare the domain at the top
 const DOMAIN = 'https://linklist-wheat.vercel.app'; // Replace with your domain
@@ -103,14 +105,7 @@ export default function ShareDialog({ uri }) {
                 className="bg-yellow-400 text-black text-lg font-bold px-6 py-4 rounded flex items-center justify-center"
                 onClick={shareOnKakao}
               >
-                <svg
-                  className="mr-2 w-5 h-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                >
-                  <path d="M12 2C6.477 2 2 5.867 2 10.5c0 2.464 1.511 4.657 3.864 6.083-.138.596-.753 3.248-.777 3.443 0 0-.015.132.066.183a.21.21 0 0 0 .214-.015c.28-.199 3.52-2.301 4.246-2.809.792.112 1.608.168 2.387.168 5.523 0 10-3.867 10-8.5S17.523 2 12 2zM8.5 11H6V8h2.5v3zm2.5-3h2V8h-2v3zm6.5 0h-1v3h-1.5V8H14v3h-1.5V8H12v3h-1.5V8H9v3H7.5V8H6v3H4.5V8H3v3h1.5v1.5H3v1h1.5v1.5H3v1h1.5V16H3v1.5H4.5V16H6v1.5H7.5V16H9v1.5h1.5V16H12v1.5h1.5V16H15v1.5h1.5V16H18v1.5h1.5V16H21v-1.5h-1.5V13H21v-1.5h-1.5V10H21v-1h-1.5V7.5H21v-1h-1.5V4.5H21v-1h-1.5V1h-1.5v1H18V.5h-1.5V1H15V.5h-1.5V1H12V.5H10.5V1H9V.5H7.5V1H6V.5H4.5V1H3v1h1.5V4.5H3v1h1.5v2.5z" />
-                </svg>
+                <Image src={kakaoLogo} alt="Kakao" width={24} height={24} className="h-6" />
                 Kakao
               </button>
               {/* Add more share buttons if needed */}
