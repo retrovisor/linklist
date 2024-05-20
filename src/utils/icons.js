23235c2f2fa3312b78013e32d8ecebf6697de0c5
retrovisor <andrenfaust@gmail.com>
2024-05-20 12:57:52 +0200

    Create icons.js

---
+++ src/utils/icons.js
@@ -0,0 +1,7 @@
+// src/utils/icons.js
+export const commonIcons = [
+  'faAddressBook', 'faAnchor', 'faAppleAlt', 'faBell', 'faBookmark',
+  'faBriefcase', 'faBullhorn', 'faCalendarAlt', 'faCamera', 'faCloudDownloadAlt',
+  'faComments', 'faEnvelope', 'faGlobeAmericas', 'faHeart', 'faHome',
+  'faLaptopCode', 'faLock', 'faNewspaper', 'faSearch', 'faShoppingCart'
+];
