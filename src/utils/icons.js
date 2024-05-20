f1525426f5154d9f95750a53464e15f7f2e12f0d
retrovisor <andrenfaust@gmail.com>
2024-05-20 17:27:19 +0200

    Update icons.js

--- src/utils/icons.js
+++ src/utils/icons.js
@@ -1,23 +1,8 @@
-// icons.js
 export const commonIcons = [
-  'faAddressBook',
-  'faAnchor',
-  'faAppleAlt',
-  'faBell',
-  'faBookmark',
-  'faBriefcase',
-  'faBullhorn',
-  'faCalendarAlt',
-  'faCamera',
-  'faCloudDownloadAlt',
-  'faComments',
-  'faEnvelope',
-  'faGlobeAmericas',
-  'faHeart',
-  'faHome',
-  'faLaptopCode',
-  'faLock',
-  'faNewspaper',
-  'faSearch',
-  'faShoppingCart',
+  'AddressBook',
+  'Anchor',
+  'AppleAlt',
+  'Bell',
+  'Bookmark'
 ];
+
