d5d8eaea36c8ec2c0562c0a3be7969fc4e781c4c
retrovisor <andrenfaust@gmail.com>
2024-05-20 13:07:16 +0200

    Update IconModal.js

--- src/components/forms/IconModal.js
+++ src/components/forms/IconModal.js
@@ -14,10 +14,10 @@ const IconModal = ({ currentIcon, onIconSelect, onClose }) => {
           {commonIcons.map((icon) => (
             <div
               key={icon}
-              className={`cursor-pointer ${currentIcon === icon ? 'text-blue-500' : 'text-gray-500'}`}
-              onClick={() => onIconSelect(icon)}
+              className={`cursor-pointer ${currentIcon === `fa-${icon}` ? 'text-blue-500' : 'text-gray-500'}`}
+              onClick={() => onIconSelect(`fa-${icon}`)}
             >
-              <FontAwesomeIcon icon={['fas', icon]} size="2x" />
+              <FontAwesomeIcon icon={`fa-solid fa-${icon}`} size="2x" />
             </div>
           ))}
         </div>
