d3edb377d043b5f76a50b795a647b267aaa2f71f
retrovisor <andrenfaust@gmail.com>
2024-05-20 17:39:51 +0200

    Update PageLinksForm.js

--- src/components/forms/PageLinksForm.js
+++ src/components/forms/PageLinksForm.js
@@ -5,16 +5,12 @@ import SectionBox from "@/components/layout/SectionBox";
 import { upload } from "@/libs/upload";
 import { faCloudArrowUp, faGripLines, faIcons, faLink, faPlus, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, far } from '@fortawesome/free-solid-svg-icons';
 import Image from "next/image";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { ReactSortable } from "react-sortablejs";
 import IconModal from "./IconModal";
-import { commonIcons } from '@/utils/icons';
-
-library.add(fas, far);
+import { commonIcons } from '@/utils/icons';  // Ensure this line exists
 
 export default function PageLinksForm({ page, user }) {
   const [links, setLinks] = useState(page.links || []);
