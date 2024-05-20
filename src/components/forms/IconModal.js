3dc495575b7bd6709f743054dfde17790da55f45
retrovisor <andrenfaust@gmail.com>
2024-05-20 12:50:23 +0200

    Update IconModal.js

--- src/components/forms/IconModal.js
+++ src/components/forms/IconModal.js
@@ -1,3 +1,5 @@
+import { commonIcons } from './PageLinksForm';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
