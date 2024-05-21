fe67e519464245d8a26806e0f3cfae60a013499d
retrovisor <andrenfaust@gmail.com>
2024-05-21 21:15:54 +0200

    Update IconModal.js

--- src/components/forms/IconModal.js
+++ src/components/forms/IconModal.js
@@ -7,7 +7,7 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fas);
 
-const IconModal = ({ currentIcon, onIconSelect, onClose, buttonRef }) => {
+const IconModal = ({ currentIcon, onIconSelect, onClose }) => {
   const modalRef = useRef();
 
   // Click outside to close modal
@@ -26,28 +26,14 @@ const IconModal = ({ currentIcon, onIconSelect, onClose, buttonRef }) => {
 
   const isCustomIcon = currentIcon.startsWith('http');
 
-  // Ensure modal is centered by updating only once and when opening
-  useEffect(() => {
-    if (buttonRef && buttonRef.current) {
-      const buttonRect = buttonRef.current.getBoundingClientRect();
-      const modalTop = window.pageYOffset + buttonRect.top - modalRef.current.offsetHeight / 2;
-      modalRef.current.style.top = `${modalTop}px`;
-    }
-  }, []);
-
   const handleIconSelect = (iconName) => {
     onIconSelect(`fa-${iconName}`);
     onClose();
-    setTimeout(() => {
-      if (buttonRef && buttonRef.current) {
-        buttonRef.current.scrollIntoView({ behavior: 'smooth' });
-      }
-    }, 0);
   };
 
   return (
     <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex items-center justify-center z-50">
-      <div ref={modalRef} className="bg-white rounded-lg shadow-lg p-4 absolute" style={{ width: '90%', maxHeight: '90vh', overflowY: 'auto' }}>
+      <div ref={modalRef} className="bg-white rounded-lg shadow-lg p-4" style={{ width: '90%', maxHeight: '90vh', overflowY: 'auto' }}>
         <div className="flex justify-between items-center border-b pb-3 mb-4">
           <h2 className="text-xl font-bold">Select Icon</h2>
           <button onClick={onClose} className="p-2">
@@ -73,7 +59,7 @@ const IconModal = ({ currentIcon, onIconSelect, onClose, buttonRef }) => {
           </div>
         )}
         <button
-          className="mt-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded fixed bottom-5 right-5"
+          className="mt-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded"
           onClick={onClose}
         >
           Close
