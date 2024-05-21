e48a49794dd820d99f7eddeee734b3c8cd5d8a95
retrovisor <andrenfaust@gmail.com>
2024-05-21 13:52:30 +0200

    Update route.js

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,35 +1,34 @@
-import { R2 } from '@cloudflare/r2';
-import uniqid from 'uniqid';
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import uniqid from "uniqid";
 
 export async function POST(req) {
   const formData = await req.formData();
-
   if (formData.has('file')) {
     const file = formData.get('file');
-
-    const r2 = new R2({
-      accountId: process.env.R2_ACCOUNT_ID,
-      accessKeyId: process.env.R2_ACCESS_KEY_ID,
-      secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+    const s3Client = new S3Client({
+      region: 'auto',
+      endpoint: process.env.R2_ENDPOINT,
+      credentials: {
+        accessKeyId: process.env.R2_ACCESS_KEY_ID,
+        secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
+      },
     });
-
     const randomId = uniqid();
     const ext = file.name.split('.').pop();
     const newFilename = `${randomId}.${ext}`;
     const bucketName = process.env.R2_BUCKET_NAME;
-
     const chunks = [];
     for await (const chunk of file.stream()) {
       chunks.push(chunk);
     }
-
-    await r2.put(newFilename, Buffer.concat(chunks), {
-      httpMetadata: {
-        contentType: file.type,
-      },
-    });
-
-    const link = `https://${bucketName}.r2.cloudflare.com/${newFilename}`;
+    await s3Client.send(new PutObjectCommand({
+      Bucket: bucketName,
+      Key: newFilename,
+      ACL: 'public-read',
+      Body: Buffer.concat(chunks),
+      ContentType: file.type,
+    }));
+    const link = `https://${bucketName}.${process.env.R2_ENDPOINT}/${newFilename}`;
     return Response.json(link);
   }
 }
