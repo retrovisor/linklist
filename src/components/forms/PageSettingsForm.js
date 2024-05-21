684f79e0ae352fb54819c2b948ccf5a7a80ed336
retrovisor <andrenfaust@gmail.com>
2024-05-21 17:23:50 +0200

    Update PageSettingsForm.js

--- src/components/forms/PageSettingsForm.js
+++ src/components/forms/PageSettingsForm.js
@@ -90,11 +90,7 @@ export default function PageSettingsForm({page,user}) {
           <div className="flex justify-center -mb-12">
             <div className="relative -top-16 w-[128px] h-[128px]">
               <div className="overflow-hidden h-full rounded-full border-4 border-white shadow shadow-black/50">
-                <Image
-                  className="w-full h-full object-cover"
-                  src={avatar}
-                  alt={'avatar'}
-                  width={128} height={128} />
+               
 
                     <Image
   className="w-full h-full object-cover"
