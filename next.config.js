5fe933c78cdea419fbbb594a19047da53e791b7f
retrovisor <andrenfaust@gmail.com>
2024-05-21 16:42:12 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
         hostname: 'linklist-files.s3.amazonaws.com',
       },
       {
+        hostname: '*.momofriends.com',
+      },
+      {
         hostname: '9057fd132931df3b08aa7529889a637f.r2.cloudflarestorage.com',
       },
       {
