5025daa481fb0bde06a84a510592a4578b489f4b
retrovisor <andrenfaust@gmail.com>
2024-05-21 14:14:28 +0200

    Update route.js

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -28,7 +28,7 @@ export async function POST(req) {
       Body: Buffer.concat(chunks),
       ContentType: file.type,
     }));
-    const link = `${process.env.R2_ENDPOINT}/${bucketName}/${newFilename}`;
-    return Response.json({ link });
+    const link = `${process.env.R2_ENDPOINT}/${newFilename}`;
+    return Response.json(link);
   }
 }
