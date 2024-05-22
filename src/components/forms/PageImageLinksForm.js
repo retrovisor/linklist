fc3abdcf413cb6c17a9c70be1939fc14e92b9544
retrovisor <andrenfaust@gmail.com>
2024-05-22 12:52:11 +0200

    Create PageImageLinksForm.js

---
+++ src/components/forms/PageImageLinksForm.js
@@ -0,0 +1,115 @@
+// components/forms/PageImageLinksForm.js
+import { saveImageLinks } from "@/actions/pageActions";
+import SubmitButton from "@/components/buttons/SubmitButton";
+import SectionBox from "@/components/layout/SectionBox";
+import { faGripLines, faPlus, faSave, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { ReactSortable } from "react-sortablejs";
+
+export default function PageImageLinksForm({ page, user }) {
+  const [imageLinks, setImageLinks] = useState(page.imageLinks || []);
+
+  async function save() {
+    await saveImageLinks(imageLinks);
+    toast.success('Saved!');
+  }
+
+  function addNewImageLink() {
+    setImageLinks(prev => [
+      ...prev,
+      {
+        key: Date.now().toString(),
+        title: '',
+        url: '',
+        linkUrl: '',
+      },
+    ]);
+  }
+
+  function handleImageLinkChange(keyOfImageLinkToChange, prop, ev) {
+    setImageLinks(prev => {
+      const newImageLinks = [...prev];
+      newImageLinks.forEach(imageLink => {
+        if (imageLink.key === keyOfImageLinkToChange) {
+          imageLink[prop] = ev.target.value;
+        }
+      });
+      return newImageLinks;
+    });
+  }
+
+  function removeImageLink(imageLinkKeyToRemove) {
+    setImageLinks(prevImageLinks =>
+      [...prevImageLinks].filter(il => il.key !== imageLinkKeyToRemove)
+    );
+  }
+
+  return (
+    <SectionBox>
+      <form action={save}>
+        <h2 className="text-2xl font-bold mb-4">Image Links</h2>
+        <button
+          onClick={addNewImageLink}
+          type="button"
+          className="text-blue-500 text-lg flex gap-2 items-center cursor-pointer"
+        >
+          <FontAwesomeIcon className="bg-blue-500 text-white p-1 rounded-full aspect-square" icon={faPlus} />
+          <span>Add new</span>
+        </button>
+        <div className="">
+          <ReactSortable handle={'.handle'} list={imageLinks} setList={setImageLinks}>
+            {imageLinks.map(il => (
+              <div key={il.key} className="mt-8">
+                <div className="handle">
+                  <FontAwesomeIcon
+                    className="text-gray-500 mr-2 cursor-ns-resize"
+                    icon={faGripLines}
+                  />
+                </div>
+                <div>
+                  <label className="input-label">Title:</label>
+                  <input
+                    value={il.title}
+                    onChange={ev => handleImageLinkChange(il.key, 'title', ev)}
+                    type="text"
+                    placeholder="Title"
+                  />
+                  <label className="input-label">Image URL:</label>
+                  <input
+                    value={il.url}
+                    onChange={ev => handleImageLinkChange(il.key, 'url', ev)}
+                    type="text"
+                    placeholder="Image URL"
+                  />
+                  <label className="input-label">Link URL:</label>
+                  <input
+                    value={il.linkUrl}
+                    onChange={ev => handleImageLinkChange(il.key, 'linkUrl', ev)}
+                    type="text"
+                    placeholder="Link URL"
+                  />
+                  <button
+                    onClick={() => removeImageLink(il.key)}
+                    type="button"
+                    className="bg-gray-300 py-2 px-3 mb-2 h-full flex gap-2 items-center justify-center"
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                    <span>Remove this image link</span>
+                  </button>
+                </div>
+              </div>
+            ))}
+          </ReactSortable>
+        </div>
+        <div className="border-t pt-4 mt-4">
+          <SubmitButton className="max-w-xs mx-auto">
+            <FontAwesomeIcon icon={faSave} />
+            <span>Save</span>
+          </SubmitButton>
+        </div>
+      </form>
+    </SectionBox>
+  );
+}
