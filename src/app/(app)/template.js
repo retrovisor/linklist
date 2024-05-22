23105a9199b48428ee1575a3155c7d42736e9f94
retrovisor <andrenfaust@gmail.com>
2024-05-22 19:47:31 +0200

    Update template.js

--- src/app/(app)/template.js
+++ src/app/(app)/template.js
@@ -52,7 +52,7 @@ export default async function AppTemplate({ children, ...rest }) {
           <label htmlFor="navCb" className="hidden backdrop fixed inset-0 bg-black/80 z-10"></label>
           <aside className="bg-white w-48 p-4 pt-6 shadow fixed md:static -left-48 top-0 bottom-0 z-20 transition-all">
             <div className="sticky top-0 pt-2">
-              <div className="rounded-full overflow-hidden aspect-square w-24 mx-auto">
+              <div className="object-cover border border-white shadow shadow-black/50 rounded-full overflow-hidden aspect-square w-24 mx-auto">
                 <Image src={session.user.image} width={256} height={256} alt={'avatar'} unoptimized />
               </div>
               {page && (
