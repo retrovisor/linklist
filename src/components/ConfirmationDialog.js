5dffdcf4b557abd5e6b5b81532ee21fd8146e31c
retrovisor <andrenfaust@gmail.com>
2024-05-23 15:26:47 +0200

    Update ConfirmationDialog.js

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -30,7 +30,7 @@ export default function ConfirmationDialog() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center">
       <div className="fixed inset-0 bg-gray-500 opacity-75"></div>
-      <div className="bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+      <div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
         <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
           <div className="sm:flex sm:items-start">
             <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
