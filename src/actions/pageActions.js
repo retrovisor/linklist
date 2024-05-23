88b89464aa7d378cf267d4ae94b1f7b52de1bbf4
retrovisor <andrenfaust@gmail.com>
2024-05-23 16:09:41 +0200

    Update pageActions.js

--- src/actions/pageActions.js
+++ src/actions/pageActions.js
@@ -158,21 +158,7 @@ export async function savePageLink(link) {
   }
 }
 
-export async function savePageLink(link) {
-  await connectToDatabase();
-  const session = await getServerSession(authOptions);
-  
-  if (session) {
-    await Page.updateOne(
-      { owner: session?.user?.email, "links.key": link.key },
-      { $set: { "links.$": link } },
-    );
-    
-    return { success: true };
-  } else {
-    return { success: false, message: 'Unauthorized' };
-  }
-}
+ 
 
 export async function saveTextBoxes(textBoxes) {
   await connectToDatabase();
