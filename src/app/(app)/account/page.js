4dd71c8f969dc1dab1a91da561e4726ad392cf97
retrovisor <andrenfaust@gmail.com>
2024-05-23 22:27:37 +0200

    Update page.js

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -61,9 +61,7 @@ export default async function AccountPage({ searchParams }) {
             <Head>
         <title>{`Edit account - ${session.user.name}`}</title>
       </Head>
-      <div className="container bg-center fixed bg-auto bg-no-repeat">
-
-   
+      <div className="container h-full bg-center fixed bg-auto overflow-x-hidden bg-no-repeat">
   
         <PageSettingsForm page={leanPage} user={session.user} />
         <PageButtonsForm page={leanPage} user={session.user} />
