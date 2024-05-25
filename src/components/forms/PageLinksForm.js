33b7cd38ae19630d976bb99706294832df0ce8a1
retrovisor <andrenfaust@gmail.com>
2024-05-25 20:18:02 +0200

    Update PageLinksForm.js

--- src/components/forms/PageLinksForm.js
+++ src/components/forms/PageLinksForm.js
@@ -1,4 +1,4 @@
-use client';
+'use client';
 import { savePageLink, deletePageLink } from "@/actions/pageActions";
 import SectionBox from "@/components/layout/SectionBox";
 import { upload } from "@/libs/upload";
