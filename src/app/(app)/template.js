a11bafd1ce382217e8509dc7f570574439de380d
retrovisor <andrenfaust@gmail.com>
2024-05-25 11:29:49 +0200

    Update template.js

--- src/app/(app)/template.js
+++ src/app/(app)/template.js
@@ -1,7 +1,7 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import AppSidebar from "@/components/layout/AppSidebar";
 import { Page } from "@/models/Page";
-import { faBars, faLink } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faLink, faChartLine, faShare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
@@ -23,11 +23,15 @@ export const metadata = {
 export default async function AppTemplate({ children, ...rest }) {
   const headersList = headers();
   const session = await getServerSession(authOptions);
+
   if (!session) {
     return redirect('/');
   }
+
   mongoose.connect(process.env.MONGO_URI);
+
   const page = await Page.findOne({ owner: session.user.email });
+
   return (
     <html lang="en">
       <body className={lato.className}>
@@ -35,21 +39,30 @@ export default async function AppTemplate({ children, ...rest }) {
         <main className="md:flex">
           <div className="flex justify-end sticky bg-white top-0 z-10">
             <label htmlFor="navCb" className="md:hidden p-2 rounded-md bg-white inline-flex items-center gap-2 cursor-pointer">
-              <div className="rounded-full overflow-hidden w-12 h-12 shadow"> {/* Ensure the image does not exceed 80px in height */}
-                <Image
-                  src={session.user.image}
-                  width={80}
-                  height={80}
-                  alt={'avatar'}
-                  unoptimized
-                  className="object-cover w-full h-full rounded-full border border-white shadow shadow-black/50"
-                />
+              <div className="flex items-center gap-2">
+                <Link href="/analytics">
+                  <FontAwesomeIcon size="lg" icon={faChartLine} className="text-blue-500" />
+                </Link>
+                <button>
+                  <FontAwesomeIcon size="lg" icon={faShare} className="text-blue-500" />
+                </button>
+                <div className="rounded-full overflow-hidden w-12 h-12 shadow"> {/* Ensure the image does not exceed 80px in height */}
+                  <Image
+                    src={session.user.image}
+                    width={80}
+                    height={80}
+                    alt={'avatar'}
+                    unoptimized
+                    className="object-cover w-full h-full rounded-full border border-white shadow shadow-black/50"
+                  />
+                </div>
               </div>
             </label>
           </div>
-          
+
           <input id="navCb" type="checkbox" className="hidden" />
           <label htmlFor="navCb" className="hidden backdrop fixed inset-0 bg-black/80 z-10"></label>
+
           <aside className="bg-white w-48 p-4 pt-6 shadow fixed md:static -left-48 top-0 bottom-0 z-20 transition-all">
             <div className="sticky top-0 pt-2">
               <div className="rounded-full overflow-hidden aspect-square w-24 mx-auto border border-white shadow shadow-black/50">
@@ -61,7 +74,8 @@ export default async function AppTemplate({ children, ...rest }) {
                   <Link
                     target="_blank"
                     href={'/' + page.uri}
-                    className="flex gap-1 items-center justify-center text-sm">
+                    className="flex gap-1 items-center justify-center text-sm"
+                  >
                     <FontAwesomeIcon size="sm" icon={faLink} className="text-blue-500" />
                     <span className="text-sm text-gray-300">/</span>
                     <span className="text-sm">{page.uri}</span>
@@ -73,6 +87,7 @@ export default async function AppTemplate({ children, ...rest }) {
               </div>
             </div>
           </aside>
+
           <div className="grow min-h-screen">
             {children}
           </div>
