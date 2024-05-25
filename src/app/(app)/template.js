6a91505b26ff7d76412920fcee3a80cd601c7bd8
retrovisor <andrenfaust@gmail.com>
2024-05-25 22:23:29 +0200

    Update template.js

--- src/app/(app)/template.js
+++ src/app/(app)/template.js
@@ -74,7 +74,7 @@ export default async function AppTemplate({ children, ...rest }) {
 
           <aside className="bg-white w-48 p-4 pt-6 shadow fixed md:static -left-48 top-0 bottom-0 z-20 transition-all">
             <div className="sticky top-0 pt-2">
-              <div className="rounded-full overflow-hidden aspect-square w-24 mx-auto border-2 border-white shadow shadow-black/50">
+              <div className="rounded-full overflow-hidden aspect-square w-24 mx-auto border-3 border-white shadow shadow-black/50">
                 <Image src={session.user.image} className="object-cover w-full h-full" width={256} height={256} alt={'avatar'} unoptimized />
               </div>
               {page && (
