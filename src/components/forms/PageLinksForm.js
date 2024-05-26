a71d69e45a624b300734aec4b5661a846df40ccd
retrovisor <andrenfaust@gmail.com>
2024-05-26 11:03:07 +0200

    Update PageLinksForm.js

--- src/components/forms/PageLinksForm.js
+++ src/components/forms/PageLinksForm.js
@@ -95,19 +95,17 @@ export default function PageLinksForm({ page, user }) {
 
   return (
     <SectionBox>
-      <div class="inline-block p-2">
-
- 
-            <h2 class="text-2xl font-bold mb-4 border-b-4 border-cyan-200">🔗 Links</h2>
-     </div>
-      <button
-        onClick={addNewLink}
-        type="button"
-        className="text-blue-500 add_button text-lg flex gap-2 items-center cursor-pointer"
-      >
-        <FontAwesomeIcon className="bg-blue-500 text-white p-1 rounded-full aspect-square" icon={faPlus} />
-        <span>Add new</span>
-      </button>
+  <div className="flex items-center justify-between mb-4">
+    <h2 className="text-2xl font-bold border-b-4 border-cyan-200 inline-block">🔗 Links</h2>
+    <button
+      onClick={addNewLink}
+      type="button"
+      className="text-blue-500 add_button text-lg flex gap-2 items-center cursor-pointer"
+    >
+      <FontAwesomeIcon className="bg-blue-500 text-white p-1 rounded-full aspect-square" icon={faPlus} />
+      <span>Add new</span>
+    </button>
+  </div>
      
 <div className="">
   <ReactSortable handle={'.handle'} list={links} setList={setLinks}>
