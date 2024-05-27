ca17e7fb00b0fb350a507cc4216447024ab1a8f7
retrovisor <andrenfaust@gmail.com>
2024-05-27 17:41:14 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,28 +1,44 @@
+import { useEffect, useState } from "react";
+import { getSession, useSession } from "next-auth/react";
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 import LoginWithFacebook from "@/components/buttons/LoginWithFacebook";
- 
+import UsernameForm from "@/components/forms/UsernameForm";
 
 export default function LoginPage() {
+  const { data: session, status } = useSession();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      setIsLoggedIn(true);
+    }
+  }, [status]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <div className="p-4 max-w-xs mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-2">
-          Sign In
-        </h1>
-        <p className="text-center mb-6 text-gray-500">
-          Sign in to your account using one of the methods below
-        </p>
-        <LoginWithKakao />
-        <div className="mt-4">
-          
-    <LoginWithGoogle />
-        </div>
-        <div className="mt-4">
-
-          <LoginWithFacebook />
-        </div>
-
+        {isLoggedIn ? (
+          <UsernameForm desiredUsername="" />
+        ) : (
+          <>
+            <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
+            <p className="text-center mb-6 text-gray-500">
+              Sign in to your account using one of the methods below
+            </p>
+            <LoginWithKakao />
+            <div className="mt-4">
+              <LoginWithGoogle />
+            </div>
+            <div className="mt-4">
+              <LoginWithFacebook />
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
