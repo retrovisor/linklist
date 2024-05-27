45dbbdb3d9f0e566e3fe900ac0c2b95df24a39b0
retrovisor <andrenfaust@gmail.com>
2024-05-27 20:23:25 +0200

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,8 @@
 import clientPromise from "@/libs/mongoClient";
-import {MongoDBAdapter} from "@auth/mongodb-adapter";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
 
 export const authOptions = {
   secret: process.env.SECRET,
@@ -9,11 +10,42 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id,
+          name: profile.kakao_account.profile.nickname,
+          email: profile.kakao_account.email,
+          image: profile.kakao_account.profile.profile_image_url.replace('http://', 'https://'),
+        };
+      },
+    }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.SECRET,
+    encryption: true,
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id;
+      return session;
+    },
+  },
 };
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
