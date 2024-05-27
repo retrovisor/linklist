970a13e1209c7b17d1e324b8aa1f6231fc1bbb55
retrovisor <andrenfaust@gmail.com>
2024-05-27 17:17:43 +0200

    Update HeroForm.js

--- src/components/forms/HeroForm.js
+++ src/components/forms/HeroForm.js
@@ -1,50 +1,47 @@
 'use client';
 
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import {signIn} from "next-auth/react";
+import {redirect, useRouter} from "next/navigation";
+import {useEffect} from "react";
 
-export default function HeroForm({ user }) {
+export default function HeroForm({user}) {
   const router = useRouter();
-
   useEffect(() => {
     if (
-      'localStorage' in window &&
-      window.localStorage.getItem('desiredUsername')
+      'localStorage' in window
+      && window.localStorage.getItem('desiredUsername')
     ) {
       const username = window.localStorage.getItem('desiredUsername');
       window.localStorage.removeItem('desiredUsername');
-      router.push('/account?desiredUsername=' + username);
+      redirect('/account?desiredUsername=' + username);
     }
   }, []);
-
   async function handleSubmit(ev) {
     ev.preventDefault();
     const form = ev.target;
     const input = form.querySelector('input');
     const username = input.value;
     if (username.length > 0) {
-      window.localStorage.setItem('desiredUsername', username);
       if (user) {
-        router.push('/account?desiredUsername=' + username);
+        router.push('/account?desiredUsername='+username);
       } else {
-        router.push('/login');  // Redirect to the login page
+        window.localStorage.setItem('desiredUsername', username);
+        await signIn('google');
       }
     }
   }
-
   return (
     <form
       onSubmit={handleSubmit}
       className="inline-flex items-center shadow-lg bg-white shadow-gray-500/20">
-      <span className="bg-white py-4 pl-4">
-        fizz.link/
-      </span>
+          <span className="bg-white py-4 pl-4">
+            linklist.to/
+          </span>
       <input
         type="text"
         className=""
-        style={{ backgroundColor: 'white', marginBottom: 0, paddingLeft: 0 }}
-        placeholder="username" />
+        style={{backgroundColor:'white',marginBottom:0,paddingLeft:0}}
+        placeholder="username"/>
       <button
         type="submit"
         className="bg-blue-500 text-white py-4 px-6 whitespace-nowrap">
