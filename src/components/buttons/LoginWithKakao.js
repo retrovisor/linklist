89c6c583a3f7fa23e5859cb3326738deeae402d4
retrovisor <andrenfaust@gmail.com>
2024-05-27 12:18:29 +0200

    Update LoginWithKakao.js

--- src/components/buttons/LoginWithKakao.js
+++ src/components/buttons/LoginWithKakao.js
@@ -1,13 +1,25 @@
 'use client';
+
 import { signIn } from "next-auth/react";
 import Image from "next/image";
-import kakaoLogo from '/public/kakao.png'; // Ensure this path is correct
+import kakaoLogo from '/public/kakao.png';
+import { useRouter } from "next/navigation";
 
 export default function LoginWithKakao() {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    const desiredUsername = window.localStorage.getItem('desiredUsername');
+    signIn('kakao', {
+      callbackUrl: desiredUsername ? `/account?desiredUsername=${desiredUsername}` : '/account',
+    });
+  };
+
   return (
     <button
-      onClick={() => signIn('kakao')}
-      className="bg-yellow-400 shadow text-center w-full py-4 flex gap-3 items-center justify-center">
+      onClick={handleLogin}
+      className="bg-yellow-400 shadow text-center w-full py-4 flex gap-3 items-center justify-center"
+    >
       <Image src={kakaoLogo} alt="Kakao" width={24} height={24} className="h-6" />
       <span>Sign In with Kakao</span>
     </button>
