dfb150f97b899e8ef7af89a4ea9147b43c5f6435
retrovisor <andrenfaust@gmail.com>
2024-05-27 12:03:29 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,27 +1,27 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import {Lato} from 'next/font/google'
-import '../globals.css'
+import { Lato } from 'next/font/google';
+import '../globals.css';
 
-const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] })
+const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
 export const metadata = {
   title: 'Fizz.link',
   description: 'Share your links, social profiles, contact info and more on one page',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={lato.className}>
-        <main>
+      <body className={`${lato.className} min-h-screen flex flex-col`}>
+        <div className="flex-grow">
           <Header />
           <div className="max-w-4xl mx-auto p-6">
             {children}
           </div>
-              <Footer />
-        </main>
+        </div>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
