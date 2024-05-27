a9da58559815291087e7d92a5b12baf3ae63b6d8
retrovisor <andrenfaust@gmail.com>
2024-05-27 12:18:42 +0200

    Update LoginWithFacebook.js

--- src/components/buttons/LoginWithFacebook.js
+++ src/components/buttons/LoginWithFacebook.js
@@ -3,11 +3,21 @@
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LoginWithFacebook() {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    const desiredUsername = window.localStorage.getItem('desiredUsername');
+    signIn('facebook', {
+      callbackUrl: desiredUsername ? `/account?desiredUsername=${desiredUsername}` : '/account',
+    });
+  };
+
   return (
     <button
-      onClick={() => signIn('facebook')}
+      onClick={handleLogin}
       className="bg-blue-600 text-white shadow text-center w-full py-4 flex gap-3 items-center justify-center"
     >
       <FontAwesomeIcon icon={faFacebook} className="h-6" />
