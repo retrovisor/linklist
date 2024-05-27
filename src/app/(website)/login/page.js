7cb59fce36a353ae21f7a4b5648dea302ab5f7a6
retrovisor <andrenfaust@gmail.com>
2024-05-27 19:49:13 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,32 +1,41 @@
+// app/(website)/login/page.js
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 import LoginWithFacebook from "@/components/buttons/LoginWithFacebook";
 import UsernameForm from "@/components/forms/UsernameForm";
+import { redirect } from "next/navigation";
 
 export default async function LoginPage({ searchParams }) {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    return <UsernameForm desiredUsername={searchParams.desiredUsername || ""} />;
+    const desiredUsername = searchParams.desiredUsername || "";
+    redirect(`/account?desiredUsername=${desiredUsername}`);
   }
+
+  const desiredUsername = searchParams.desiredUsername || "";
 
   return (
     <div>
-      <div className="p-4 max-w-xs mx-auto">
-        <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
-        <p className="text-center mb-6 text-gray-500">
-          Sign in to your account using one of the methods below
-        </p>
-        <LoginWithKakao />
-        <div className="mt-4">
-          <LoginWithGoogle />
+      {desiredUsername ? (
+        <UsernameForm desiredUsername={desiredUsername} />
+      ) : (
+        <div className="p-4 max-w-xs mx-auto">
+          <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
+          <p className="text-center mb-6 text-gray-500">
+            Sign in to your account using one of the methods below
+          </p>
+          <LoginWithKakao />
+          <div className="mt-4">
+            <LoginWithGoogle />
+          </div>
+          <div className="mt-4">
+            <LoginWithFacebook />
+          </div>
         </div>
-        <div className="mt-4">
-          <LoginWithFacebook />
-        </div>
-      </div>
+      )}
     </div>
   );
 }
