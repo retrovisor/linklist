24e3220dfa0c7b339ec540bd5046997349c5e013
retrovisor <andrenfaust@gmail.com>
2024-05-27 17:13:35 +0200

    Update Page.js

--- src/models/Page.js
+++ src/models/Page.js
@@ -1,39 +1,16 @@
-import { model, models, Schema } from "mongoose";
+import {model, models, Schema} from "mongoose";
 
 const PageSchema = new Schema({
-  uri: { type: String, required: true, min: 1, unique: true },
-  owner: { type: String, required: true },
-  displayName: { type: String, default: '' },
-  location: { type: String, default: '' },
-  bio: { type: String, default: '' },
-  bgType: { type: String, default: 'color' },
-  bgColor: { type: String, default: '#000' },
-  bgImage: { type: String, default: '' },
-  buttons: { type: Object, default: {} },
-  links: { type: Array, default: [] },
-  template: { type: String, default: '' },
-  textBoxes: [
-    {
-      key: String,
-      title: String,
-      text: String,
-    },
-  ],
-  imageLinks: [
-    {
-      key: String,
-      title: String,
-      url: String,
-      linkUrl: String,
-    },
-  ],
-
-   youTubeVideos: [
-    {
-      key: String,
-      url: String,
-    },
-  ],
-}, { timestamps: true });
+  uri: {type: String, required: true, min: 1, unique: true},
+  owner: {type: String, required: true},
+  displayName: {type: String, default: ''},
+  location: {type: String, default: ''},
+  bio: {type: String, default: ''},
+  bgType: {type: String, default: 'color'},
+  bgColor: {type: String, default: '#000'},
+  bgImage: {type: String, default: ''},
+  buttons: {type: Object, default: {}},
+  links: {type: Object, default: []},
+}, {timestamps: true});
 
 export const Page = models?.Page || model('Page', PageSchema);
