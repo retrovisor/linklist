810cf38162f81954426e5d16db2d748902a66e84
retrovisor <andrenfaust@gmail.com>
2024-05-27 17:03:56 +0200

    Update LoginWithGoogle.js

--- src/components/buttons/LoginWithGoogle.js
+++ src/components/buttons/LoginWithGoogle.js
@@ -1,25 +1,13 @@
 'use client';
-
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { signIn } from "next-auth/react";
-import { useRouter } from "next/navigation";
+import {faGoogle} from "@fortawesome/free-brands-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {signIn} from "next-auth/react";
 
 export default function LoginWithGoogle() {
-  const router = useRouter();
-
-  const handleLogin = () => {
-    const desiredUsername = window.localStorage.getItem('desiredUsername');
-    signIn('google', {
-      callbackUrl: desiredUsername ? `/account?desiredUsername=${desiredUsername}` : '/account',
-    });
-  };
-
   return (
     <button
-      onClick={handleLogin}
-      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center"
-    >
+      onClick={() => signIn('google')}
+      className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center">
       <FontAwesomeIcon icon={faGoogle} className="h-6" />
       <span>Sign In with Google</span>
     </button>
