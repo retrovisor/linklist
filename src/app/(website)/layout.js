5e914e13bb0a0d7384e162afb793c40c60a8dc3b
retrovisor <andrenfaust@gmail.com>
2024-05-28 19:07:03 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${lato.className} min-h-screen flex flex-col`}>
+      <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <div className="flex-grow">
           <Header />
           <div className="max-w-4xl mx-auto p-6">
