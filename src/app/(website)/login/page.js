d55d0f02a1e6b6e8727b7511cc25b27e1167ad11
retrovisor <andrenfaust@gmail.com>
2024-05-28 14:10:37 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,45 +1,29 @@
-// app/(website)/login/page.js
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 import LoginWithFacebook from "@/components/buttons/LoginWithFacebook";
-import UsernameForm from "@/components/forms/UsernameForm";
-import { redirect } from "next/navigation";
+ 
 
-export default async function LoginPage({ searchParams }) {
-  console.log('LoginPage function started');
-
-  try {
-    const session = await getServerSession(authOptions);
-    console.log('Session:', session);
-
-    if (session) {
-      console.log('User is logged in, rendering UsernameForm');
-      const desiredUsername = searchParams.desiredUsername || "";
-      return <UsernameForm desiredUsername={desiredUsername} />;
-    }
-
-    console.log('User is not logged in, rendering login buttons');
-    return (
-      <div>
-        <div className="p-4 max-w-xs mx-auto">
-          <h1 className="text-4xl font-bold text-center mb-2">Sign In</h1>
-          <p className="text-center mb-6 text-gray-500">
-            Sign in to your account using one of the methods below
-          </p>
-          <LoginWithKakao />
-          <div className="mt-4">
-            <LoginWithGoogle />
-          </div>
-          <div className="mt-4">
-            <LoginWithFacebook />
-          </div>
+export default function LoginPage() {
+  return (
+    <div>
+      <div className="p-4 max-w-xs mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-2">
+          Sign In
+        </h1>
+        <p className="text-center mb-6 text-gray-500">
+          Sign in to your account using one of the methods below
+        </p>
+        <LoginWithKakao />
+        <div className="mt-4">
+          
+    <LoginWithGoogle />
         </div>
+        <div className="mt-4">
+
+          <LoginWithFacebook />
+        </div>
+
       </div>
-    );
-  } catch (error) {
-    console.error('Error in LoginPage:', error);
-    return <div>An error occurred. Please try again later.</div>;
-  }
+    </div>
+  );
 }
