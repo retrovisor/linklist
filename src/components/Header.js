2222459bb4706b85766ad8b51fa64a4944a5a315
retrovisor <andrenfaust@gmail.com>
2024-05-28 19:14:20 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,14 @@
-
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LogoutButton from "@/components/buttons/LogoutButton";
-import {faLink} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {getServerSession} from "next-auth";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
 export default async function Header() {
   const session = await getServerSession(authOptions);
   return (
-    <header className="bg-white border-b py-2">
+    <header className="bg-white border-b py-2 sticky top-0 z-50">
       <div className="max-w-4xl flex justify-between mx-auto px-4">
         <div className="flex items-center gap-6">
           <Link href={'/'} className="flex items-center gap-1 text-blue-500">
