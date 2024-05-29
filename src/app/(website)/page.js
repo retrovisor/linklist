33f1d7f59163491c3774385054833ce7201c172f
retrovisor <andrenfaust@gmail.com>
2024-05-29 13:38:58 +0200

    Update page.js

--- src/app/(website)/page.js
+++ src/app/(website)/page.js
@@ -12,7 +12,8 @@ export default async function Home() {
 
 
      <div className="testimonial1-item">
-    <div className="testimonial1-logo-wrap"><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating"><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating"><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating"><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating"><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating">
+    <div className="testimonial1-logo-wrap">
+   <img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating" /><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating" /><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating" /><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating" /><img src="https://assets-global.website-files.com/65fdc14a5172c12ead07d01d/65fdc14b5172c12ead07d07c_star-filled.svg" width="24" height="24" alt="" className="star-rating" /> 
          </div>
     </div>
     
@@ -27,12 +28,13 @@ export default async function Home() {
     
       <section className="py-16">
         <div className="max-w-md mb-8">
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-5xl text-center font-bold">
             Your one link<br />for everything
           </h1>
-          <h2 className="text-gray-500 text-xl mt-6">
+          <h2 className="text-gray-500 text-xl text-center mt-6">
             Share your links, social profiles, contact info and more on a single page
           </h2>
+    <p className="text-bold">It's free! 🎉</p>
         </div>
         <HeroForm user={session?.user} />
       </section>
