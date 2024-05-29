9c35859b015a514d08b16d46a37d9d07514d26a5
retrovisor <andrenfaust@gmail.com>
2024-05-29 19:52:34 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <div className="flex-grow">
           <Header />
-          <div className="max-w-4xl mx-auto p-6">
+          <div className="mx-auto">
             {children}
           </div>
         </div>
