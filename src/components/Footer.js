9a7dee47364a664d5c2cc0843747ddb4c5a82c49
retrovisor <andrenfaust@gmail.com>
2024-05-29 19:42:25 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,29 +13,14 @@ export default async function Footer() {
       <div className="max-w-4xl flex justify-between mx-auto px-4">
         <div className="flex items-center gap-6">
           <Link href={'/'} className="flex items-center gap-1 text-blue-500">
-            <FontAwesomeIcon icon={faLink} className="text-blue-500" />
+            <FontAwesomeIcon icon={faLink} className="cor-roxa" />
             <span className="font-bold">Fizz.link</span>
           </Link>
           <nav className="flex items-center gap-4 text-slate-500 text-sm">
             <Link href={'/about'}>About</Link>
            </nav>
         </div>
-        <nav className="flex items-center gap-4 text-sm text-slate-500">
-          {!!session && (
-            <>
-              <Link href={'/account'}>
-                My Links
-              </Link>
-              <LogoutButton />
-            </>
-          )}
-          {!session && (
-            <>
-              <Link href={'/login'}>Sign In</Link>
-              <Link href={'/login'}>Create Account</Link>
-            </>
-          )}
-        </nav>
+         
       </div>
     </footer>
   );
