0e46b50d4a085084a78ffeeeae6a62aa445962ea
retrovisor <andrenfaust@gmail.com>
2024-05-29 13:48:20 +0200

    Update page.js

--- src/app/(website)/page.js
+++ src/app/(website)/page.js
@@ -1,31 +1,29 @@
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import HeroForm from "@/components/forms/HeroForm";
-import {getServerSession} from "next-auth";
+import { getServerSession } from "next-auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+
   return (
     <main>
+      <div className="w-layout-hflex flex-block">
+        <div>
+          <div className="testimonial1-item">
+            <div className="testimonial1-logo-wrap">
+              <FontAwesomeIcon icon={faStar} color="#FFD700" className="star-rating" />
+              <FontAwesomeIcon icon={faStar} color="#FFD700" className="star-rating" />
+              <FontAwesomeIcon icon={faStar} color="#FFD700" className="star-rating" />
+              <FontAwesomeIcon icon={faStar} color="#FFD700" className="star-rating" />
+              <FontAwesomeIcon icon={faStar} color="#FFD700" className="star-rating" />
+            </div>
+          </div>
+          <div className="text-center">&ldquo;Best wishlist page I&apos;ve used&rdquo;</div>
+        </div>
+      </div>
 
-<div className="w-layout-hflex flex-block">
-    <div>
-
-
-     <div className="testimonial1-item">
-    <div className="testimonial1-logo-wrap">
-   
-         </div>
-    </div>
-    
-    
-
-     <div className="text-center">"Best wishlist page I've used"</div>
-
-    </div>
-    </div>
- 
-
-    
       <section className="py-16">
         <div className="max-w-md mb-8">
           <h1 className="text-5xl text-center font-bold">
@@ -34,12 +32,12 @@ export default async function Home() {
           <h2 className="text-gray-500 text-xl text-center mt-6">
             Share your links, social profiles, contact info and more on a single page
           </h2>
-    <p className="text-bold">It's free! 🎉</p>
+          <p className="text-bold">It&apos;s free! 🎉</p>
         </div>
         <HeroForm user={session?.user} />
       </section>
 
-  <section className="py-16">
+      <section className="py-16">
         <div className="max-w-md mb-8">
           <h1 className="text-5xl font-bold bg-blue-200">
             Bilu-bily
@@ -51,5 +49,5 @@ export default async function Home() {
         <HeroForm user={session?.user} />
       </section>
     </main>
-  )
+  );
 }
