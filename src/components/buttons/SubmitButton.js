19adbef87525d8bf2627d1e9db2fafeae331edb7
retrovisor <andrenfaust@gmail.com>
2024-05-30 18:03:51 +0200

    Update SubmitButton.js

--- src/components/buttons/SubmitButton.js
+++ src/components/buttons/SubmitButton.js
@@ -6,7 +6,7 @@ export default function SubmitButton({children, className=''}) {
     <button
       type="submit"
       disabled={pending}
-      className={"bg-blue-500 disabled:bg-blue-400 text-white disabled:text-gray-200 py-2 px-4 block mx-auto w-full flex gap-2 items-center justify-center " + className}
+      className={"btn-link disabled:bg-blue-400 text-white disabled:text-gray-200 py-2 px-4 block mx-auto w-full flex gap-2 items-center justify-center " + className}
     >
       {pending && (
         <span>Saving...</span>
