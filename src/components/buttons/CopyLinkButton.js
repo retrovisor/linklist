c877dd32b48f149bc73c57e98b9542350609bf50
retrovisor <andrenfaust@gmail.com>
2024-05-31 14:31:14 +0200

    Update CopyLinkButton.js

--- src/components/buttons/CopyLinkButton.js
+++ src/components/buttons/CopyLinkButton.js
@@ -17,7 +17,7 @@ const CopyLinkButton = ({ uri }) => {
 
   return (
     <button
-      className="flex items-center justify-center px-3 py-1 rounded-md bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+      className="flex items-center justify-center px-3 py-3 rounded-md bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
       onClick={copyLink}
     >
       <FontAwesomeIcon icon={faCopy} className="mr-2" />
