76d79e6c9d02d14070830e4e48e5593f71030123
retrovisor <andrenfaust@gmail.com>
2024-06-01 12:18:50 +0200

    Create next-i18next.config.js

---
+++ next-i18next.config.js
@@ -0,0 +1,11 @@
+// next-i18next.config.js
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ko'],
+    localeSubpaths: {
+      en: 'en',
+      ko: 'ko',
+    },
+  },
+};
