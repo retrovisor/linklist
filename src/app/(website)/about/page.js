eec560932f2984b9730efa92c08a0ef3e12a3fca
retrovisor <andrenfaust@gmail.com>
2024-06-01 19:17:05 +0200

    Update page.js

--- src/app/(website)/about/page.js
+++ src/app/(website)/about/page.js
@@ -2,18 +2,19 @@ import Link from 'next/link';
 
 export default function AboutPage() {
   return (
-    <div className="bg-blue-950 text-white min-h-screen">
+    <div className="bg-white text-white min-h-screen">
       <div className="h-36 bg-gray-400 bg-cover bg-center">
         <h1 className="text-4xl text-center pt-12">About Page</h1>
       </div>
       <div className="max-w-xs mx-auto text-center my-4">
-        <p>Welcome to the about page. Here you will find information about our website and mission.</p>
-        {/* Add more static content as needed */}
+        <p>At Fizz.link, we believe that every link you share is a chapter of your unique journey. We're more than just a tool; we're your creative space on the web where you can bring together all your important links, social profiles, and projects on one beautifully simple page. But it doesn’t end there.</p>
+        
+  <p>We understand that you're not just a set of links. You’re a personality, an individual with preferences, passions, and stories to tell. Fizz.link invites you to express who you truly are. Here, you can share your favorite quotes, showcase your hobbies, or tell a little story about what makes you, well, you.</p>
+    <p>Whether you’re a professional looking to consolidate your portfolio, a creator eager to share your latest works, or someone who just wants to make a personal statement on the digital expanse, Fizz.link is here to make sure that your digital presence resonates with both your work and your character.</p>
+    <p>Create, customize, and connect with Fizz.link – because every link should tell your story.</p>
       </div>
       <div className="max-w-2xl mx-auto grid md:grid-cols-2 gap-6 p-4 px-8">
-        <Link href="/">
-          <a className="bg-indigo-800 p-2 block flex items-center justify-center text-white">Go back to Home</a>
-        </Link>
+        
       </div>
     </div>
   );
