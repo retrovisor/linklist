096f190633f1962e1636e4e17c02d373a9e8e4c1
retrovisor <andrenfaust@gmail.com>
2024-06-02 11:02:36 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,7 +1,9 @@
+
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
+import { appWithTranslation } from 'next-i18next';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -10,7 +12,7 @@ export const metadata = {
   description: 'Share your links, social profiles, contact info and more on one page',
 };
 
-export default function RootLayout({ children }) {
+function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
@@ -25,3 +27,5 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
+export default appWithTranslation(RootLayout);
