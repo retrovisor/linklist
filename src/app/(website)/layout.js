38eae68540538ad48978b0dd4f6de3497e1f5bad
retrovisor <andrenfaust@gmail.com>
2024-06-02 11:38:14 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -3,8 +3,8 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
-import { useRouter } from 'next/router';
-import { IntlProvider } from 'react-intl'; 
+import { useLocale } from 'next-intl';
+import { IntlProvider } from 'react-intl';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -13,31 +13,21 @@ export const metadata = {
   description: 'Share your links, social profiles, contact info and more on one page',
 };
 
-export default function RootLayout({ children }) {
-  const router = useRouter();
-  const { locale } = router;
+export default async function RootLayout({ children }) {
+  const locale = useLocale();
 
-  const messages = {
-    en: {
-      aboutUsTitle: 'About Us',
-      paragraph1: 'This is the first paragraph.',
-      paragraph2: 'This is the second paragraph.',
-      paragraph3: 'This is the third paragraph.',
-      paragraph4: 'This is the fourth paragraph.',
-    },
-    ko: {
-      aboutUsTitle: '회사 소개',
-      paragraph1: '첫 번째 단락입니다.',
-      paragraph2: '두 번째 단락입니다.',
-      paragraph3: '세 번째 단락입니다.',
-      paragraph4: '네 번째 단락입니다.',
-    },
-  };
+  let messages;
+  try {
+    messages = await import(`../../../translations/${locale}.json`);
+  } catch (error) {
+    // Fallback to default locale if translations are not found
+    messages = await import(`../../../translations/en.json`);
+  }
 
   return (
     <html lang={locale}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
-        <IntlProvider locale={locale} messages={messages[locale]}>
+        <IntlProvider locale={locale} messages={messages.default}>
           <div className="flex-grow">
             <Header />
             <div className="mx-auto">
