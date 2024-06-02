a453d70f930d12126a62ef0c1639b21abf51d776
retrovisor <andrenfaust@gmail.com>
2024-06-02 20:44:42 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,4 +1,4 @@
-import { useTranslation } from 'next-i18next';
+import { appWithTranslation } from 'next-i18next';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
@@ -6,18 +6,12 @@ import '../globals.css';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export async function generateStaticParams() {
-  return [{ locale: 'en' }, { locale: 'ko' }];
-}
-
 export const metadata = {
   title: 'Fizz.link - Your one link for everything',
   description: 'Share your links, social profiles, contact info and more on one page',
 };
 
-export default async function RootLayout({ children, params: { locale } }) {
-  const { i18n } = await useTranslation(locale);
-
+function RootLayout({ children, params: { locale } }) {
   return (
     <html lang={locale}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
@@ -31,4 +25,10 @@ export default async function RootLayout({ children, params: { locale } }) {
       </body>
     </html>
   );
+}
+
+export default appWithTranslation(RootLayout);
+
+export async function generateStaticParams() {
+  return [{ locale: 'en' }, { locale: 'ko' }];
 }
