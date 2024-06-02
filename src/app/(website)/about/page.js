67a5ba718da53f98d4871bde5dd8593ccdbd5e8d
retrovisor <andrenfaust@gmail.com>
2024-06-02 20:24:57 +0200

    Update page.js

--- src/app/(website)/about/page.js
+++ src/app/(website)/about/page.js
@@ -1,40 +1,33 @@
-// src/app/(website)/about/page.js
-export async function generateStaticParams() {
-  return [
-    { locale: 'en' },
-    { locale: 'ko' },
-  ];
-}
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export default function AboutPage({ locale }) {
-  const effectiveLocale = locale || 'en';
-
-  let translations = {};
-  try {
-    const translationsModule = require(`../../../../translations/${effectiveLocale}.json`);
-    translations = translationsModule.default || translationsModule;
-  } catch (error) {
-    console.error('Error loading translations:', error);
-    const translationsModule = require(`../../../../translations/en.json`);
-    translations = translationsModule.default || translationsModule;
-  }
+export default function AboutPage() {
+  const { t } = useTranslation('common');
 
   return (
     <div className="bg-white text-white min-h-screen">
       <div className="h-36 colorido bg-cover bg-center">
         <h1 className="text-5xl font-bold text-center text-white pt-12">
-          {translations.aboutUsTitle}
+          {t('aboutUsTitle')}
         </h1>
       </div>
       <div className="max-w-xl mx-auto text-center my-4 text-black px-4 text-left">
-        <p className="p-4 text-left">{translations.paragraph1}</p>
-        <p className="p-4 text-left">{translations.paragraph2}</p>
-        <p className="p-4 text-left">{translations.paragraph3}</p>
-        <p className="p-4 text-left">{translations.paragraph4}</p>
+        <p className="p-4 text-left">{t('paragraph1')}</p>
+        <p className="p-4 text-left">{t('paragraph2')}</p>
+        <p className="p-4 text-left">{t('paragraph3')}</p>
+        <p className="p-4 text-left">{t('paragraph4')}</p>
       </div>
       <div className="max-w-2xl mx-auto grid md:grid-cols-2 gap-6 p-4 px-8">
         {/* Content for the grid */}
       </div>
     </div>
   );
+}
+
+export async function getStaticProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+  };
 }
