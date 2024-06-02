5ef77ed6fc0985d8617fbeb271c0e7fb29cd477e
retrovisor <andrenfaust@gmail.com>
2024-06-02 11:49:43 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -3,7 +3,8 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
-import { IntlProvider } from 'react-intl';
+import { useTranslation } from 'react-i18next';
+import i18n from '../../../i18n';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -13,26 +14,19 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children, params: { locale } }) {
-  let messages;
-  try {
-    messages = await import(`../../../translations/${locale}.json`);
-  } catch (error) {
-    // Fallback to default locale if translations are not found
-    messages = await import(`../../../translations/en.json`);
-  }
+  const { i18n } = useTranslation();
+  i18n.changeLanguage(locale);
 
   return (
     <html lang={locale}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
-        <IntlProvider locale={locale} messages={messages.default}>
-          <div className="flex-grow">
-            <Header />
-            <div className="mx-auto">
-              {children}
-            </div>
+        <div className="flex-grow">
+          <Header />
+          <div className="mx-auto">
+            {children}
           </div>
-          <Footer />
-        </IntlProvider>
+        </div>
+        <Footer />
       </body>
     </html>
   );
