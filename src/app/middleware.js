83687cb92d26270f8be814b3bad6060301c1f0c6
retrovisor <andrenfaust@gmail.com>
2024-06-03 17:37:53 +0200

    Update middleware.js

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,22 +1,49 @@
-// src/middleware.js
+import { NextResponse } from 'next/server';
+import acceptLanguage from 'accept-language';
+import { fallbackLng, languages, cookieName } from './app/i18n/settings';
 
-import { getToken } from 'next-auth/jwt'
-import { NextResponse } from 'next/server'
-
-export async function middleware(req) {
-  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-
-  const { pathname } = req.nextUrl
-
-  // If the user is logged in and trying to access the login page, redirect them to /account
-  if (token && pathname === '/login') {
-    return NextResponse.redirect(new URL('/account', req.url))
-  }
-
-  // Continue with the request if it's not a login page or if the user is not logged in
-  return NextResponse.next()
-}
+acceptLanguage.languages(languages);
 
 export const config = {
-  matcher: '/login'
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)'],
+};
+
+export function middleware(req) {
+  const { pathname } = req.nextUrl;
+
+  // Check if the path starts with a locale
+  const isLocalePath = languages.some((loc) => pathname.startsWith(`/${loc}`));
+  const isUserGeneratedPath = pathname.split('/').length === 2 && !isLocalePath;
+
+  // Handle locale-prefixed paths
+  if (isLocalePath) {
+    const locale = pathname.split('/')[1];
+    const response = NextResponse.next();
+    response.cookies.set(cookieName, locale);
+    return response;
+  }
+
+  // Handle user-generated paths
+  if (isUserGeneratedPath) {
+    return NextResponse.rewrite(new URL(`/user/${pathname.slice(1)}`, req.url));
+  }
+
+  // Detect preferred language if no locale in path
+  let lng;
+  if (req.cookies.has(cookieName)) {
+    lng = acceptLanguage.get(req.cookies.get(cookieName).value);
+  }
+  if (!lng) {
+    lng = acceptLanguage.get(req.headers.get('Accept-Language'));
+  }
+  if (!lng) {
+    lng = fallbackLng;
+  }
+
+  // Redirect to locale-prefixed path if not already
+  if (!isLocalePath) {
+    return NextResponse.redirect(new URL(`/${lng}${pathname}`, req.url));
+  }
+
+  return NextResponse.next();
 }
