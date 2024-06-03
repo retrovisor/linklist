d0fc3a016c2207e83667e6eab117b119f92fb826
retrovisor <andrenfaust@gmail.com>
2024-06-03 17:19:55 +0200

    Update and rename layout.js to layout.js

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -1,21 +1,24 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
-import { dir } from 'i18next';
 import '../../globals.css';
+import { dir } from 'i18next';
+import { languages } from '../i18n/settings';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
-
-export async function generateStaticParams() {
-  return ['en', 'ko'].map(locale => ({ locale }));
-}
 
 export const metadata = {
   title: 'Fizz.link - Your one link for everything',
   description: 'Share your links, social profiles, contact info and more on one page',
 };
 
-export default function RootLayout({ children, params: { locale } }) {
+export async function generateStaticParams() {
+  return languages.map((locale) => ({ locale }));
+}
+
+export default function RootLayout({ children, params }) {
+  const { locale } = params;
+
   return (
     <html lang={locale} dir={dir(locale)}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
