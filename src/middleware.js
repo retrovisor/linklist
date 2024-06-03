8fc1e1a35a8ec80c6b6501e1f6673fa0d7669aaf
retrovisor <andrenfaust@gmail.com>
2024-06-03 17:33:09 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,3 @@
-// src/middleware.js
 import { NextResponse } from 'next/server';
 import acceptLanguage from 'accept-language';
 import { fallbackLng, languages, cookieName } from './app/i18n/settings';
@@ -6,19 +5,17 @@ import { fallbackLng, languages, cookieName } from './app/i18n/settings';
 acceptLanguage.languages(languages);
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)',
-    '/:locale(en|ko)?/:path*',
-    '/:uri',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)'],
 };
 
 export function middleware(req) {
   const { pathname } = req.nextUrl;
 
+  // Check if the path starts with a locale
   const isLocalePath = languages.some((loc) => pathname.startsWith(`/${loc}`));
   const isUserGeneratedPath = pathname.split('/').length === 2;
 
+  // Handle locale-prefixed paths
   if (isLocalePath) {
     const locale = pathname.split('/')[1];
     const response = NextResponse.next();
@@ -26,10 +23,12 @@ export function middleware(req) {
     return response;
   }
 
+  // Handle user-generated paths
   if (isUserGeneratedPath) {
     return NextResponse.rewrite(new URL(`/[uri]${pathname}`, req.url));
   }
 
+  // Detect preferred language if no locale in path
   let lng;
   if (req.cookies.has(cookieName)) {
     lng = acceptLanguage.get(req.cookies.get(cookieName).value);
@@ -41,6 +40,7 @@ export function middleware(req) {
     lng = fallbackLng;
   }
 
+  // Redirect to locale-prefixed path if not already
   if (!isLocalePath) {
     return NextResponse.redirect(new URL(`/${lng}${pathname}`, req.url));
   }
