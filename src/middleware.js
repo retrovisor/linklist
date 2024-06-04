b1d78bec77ac990083b4f60232b94e4e17c85545
retrovisor <andrenfaust@gmail.com>
2024-06-04 23:01:12 +0200

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
-// src/app/middleware.js
+// src/middleware.js
 import { NextResponse } from 'next/server';
 import acceptLanguage from 'accept-language';
-import { fallbackLng, languages, cookieName } from './i18n/settings';
+import { fallbackLng, languages, cookieName } from './app/i18n/settings';
 
 acceptLanguage.languages(languages);
 
@@ -12,47 +12,37 @@ export const config = {
 };
 
 export function middleware(req) {
-  try {
-    const { pathname } = req.nextUrl;
-    console.log('Pathname:', pathname);
+  const pathname = req.nextUrl.pathname;
+  const pathnameIsMatcher = pathname.startsWith('/api') || pathname.startsWith('/_next') || pathname.startsWith('/assets') || pathname.startsWith('/favicon.ico') || pathname.startsWith('/sw.js') || pathname.startsWith('/site.webmanifest');
 
-    const isLocalePath = languages.some((loc) => pathname.startsWith(`/${loc}/`));
-    console.log('Is Locale Path:', isLocalePath);
-
-    if (isLocalePath) {
-      const locale = pathname.split('/')[1];
-      console.log('Locale:', locale);
-      const response = NextResponse.next();
-      response.cookies.set(cookieName, locale);
-      console.log('Setting locale cookie:', locale);
-      return response;
-    }
-
-    let lng;
-    if (req.cookies.has(cookieName)) {
-      lng = acceptLanguage.get(req.cookies.get(cookieName).value);
-      console.log('Language from cookie:', lng);
-    }
-    if (!lng) {
-      lng = acceptLanguage.get(req.headers.get('Accept-Language'));
-      console.log('Language from Accept-Language header:', lng);
-    }
-    if (!lng) {
-      lng = fallbackLng;
-      console.log('Fallback language:', lng);
-    }
-
-    if (!isLocalePath) {
-      const uriPath = pathname.slice(1);
-      const rewriteUrl = new URL(`/${lng}/[uri]/${uriPath}`, req.url);
-      console.log('Rewriting URL:', rewriteUrl.toString());
-      return NextResponse.rewrite(rewriteUrl);
-    }
-
-    console.log('Continuing to next middleware or route handler');
+  if (pathnameIsMatcher) {
     return NextResponse.next();
-  } catch (error) {
-    console.error('Middleware Error:', error);
-    return NextResponse.error();
   }
+
+  const isLocalePath = languages.some((loc) => pathname.startsWith(`/${loc}`));
+
+  if (isLocalePath) {
+    const locale = pathname.split('/')[1];
+    const response = NextResponse.next();
+    response.cookies.set(cookieName, locale);
+    return response;
+  }
+
+  let lng;
+  if (req.cookies.has(cookieName)) {
+    lng = acceptLanguage.get(req.cookies.get(cookieName).value);
+  }
+  if (!lng) {
+    lng = acceptLanguage.get(req.headers.get('Accept-Language'));
+  }
+  if (!lng) {
+    lng = fallbackLng;
+  }
+
+  if (!isLocalePath) {
+    const uri = pathname.substring(1);
+    return NextResponse.rewrite(new URL(`/${lng}/(page)/${uri}`, req.url));
+  }
+
+  return NextResponse.next();
 }
