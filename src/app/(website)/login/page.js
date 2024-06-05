11ab3ca39f56c44cda0e61796a026588ebadff6e
retrovisor <andrenfaust@gmail.com>
2024-06-05 16:21:14 +0200

    Update page.js

--- src/app/(website)/login/page.js
+++ src/app/(website)/login/page.js
@@ -1,4 +1,3 @@
-// app/(website)/login/page.js
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
@@ -6,6 +5,8 @@ import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 import LoginWithFacebook from "@/components/buttons/LoginWithFacebook";
 import UsernameForm from "@/components/forms/UsernameForm";
 import { redirect } from "next/navigation";
+import { User } from "@/models/User";
+import mongoose from "mongoose";
 
 export default async function LoginPage({ searchParams }) {
   console.log('LoginPage function started');
@@ -15,7 +16,21 @@ export default async function LoginPage({ searchParams }) {
     console.log('Session:', session);
 
     if (session) {
-      console.log('User is logged in, rendering UsernameForm');
+      console.log('User is logged in');
+
+      // Ensure mongoose connection is established only once
+      if (mongoose.connection.readyState === 0) {
+        await mongoose.connect(process.env.MONGO_URI);
+      }
+
+      // Check if the user has already picked a username
+      const user = await User.findOne({ email: session?.user?.email });
+      if (user && user.username) {
+        console.log('User has already picked a username, redirecting to /account');
+        return redirect('/account');
+      }
+
+      console.log('User has not picked a username, rendering UsernameForm');
       const desiredUsername = searchParams.desiredUsername || "";
       return <UsernameForm desiredUsername={desiredUsername} />;
     }
