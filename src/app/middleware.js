82c2f9d95813838edacded0e34a251534a193f19
retrovisor <andrenfaust@gmail.com>
2024-06-05 11:53:13 +0200

    Update middleware.js

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,48 +1,22 @@
 // src/middleware.js
-import { NextResponse } from 'next/server';
-import acceptLanguage from 'accept-language';
-import { fallbackLng, languages, cookieName } from './app/i18n/settings';
 
-acceptLanguage.languages(languages);
+import { getToken } from 'next-auth/jwt'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+
+  const { pathname } = req.nextUrl
+
+  // If the user is logged in and trying to access the login page, redirect them to /account
+  if (token && pathname === '/login') {
+    return NextResponse.redirect(new URL('/account', req.url))
+  }
+
+  // Continue with the request if it's not a login page or if the user is not logged in
+  return NextResponse.next()
+}
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)',
-  ],
-};
-
-export function middleware(req) {
-  const pathname = req.nextUrl.pathname;
-  const pathnameIsMatcher = pathname.startsWith('/api') || pathname.startsWith('/_next') || pathname.startsWith('/assets') || pathname.startsWith('/favicon.ico') || pathname.startsWith('/sw.js') || pathname.startsWith('/site.webmanifest');
-
-  if (pathnameIsMatcher) {
-    return NextResponse.next();
-  }
-
-  const isLocalePath = languages.some((loc) => pathname.startsWith(`/${loc}`));
-
-  if (isLocalePath) {
-    const locale = pathname.split('/')[1];
-    const response = NextResponse.next();
-    response.cookies.set(cookieName, locale);
-    return response;
-  }
-
-  let lng;
-  if (req.cookies.has(cookieName)) {
-    lng = acceptLanguage.get(req.cookies.get(cookieName).value);
-  }
-  if (!lng) {
-    lng = acceptLanguage.get(req.headers.get('Accept-Language'));
-  }
-  if (!lng) {
-    lng = fallbackLng;
-  }
-
-  if (!isLocalePath) {
-    const uri = pathname.substring(1);
-    return NextResponse.rewrite(new URL(`/${lng}/(page)/${uri}`, req.url));
-  }
-
-  return NextResponse.next();
+  matcher: '/login'
 }
