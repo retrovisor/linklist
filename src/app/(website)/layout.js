80bfa379cddb7bb944110a40acbf959a0336afbd
retrovisor <andrenfaust@gmail.com>
2024-06-05 11:56:35 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,4 +1,3 @@
-import { appWithTranslation } from 'next-i18next';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
@@ -11,24 +10,18 @@ export const metadata = {
   description: 'Share your links, social profiles, contact info and more on one page',
 };
 
-function RootLayout({ children, params: { locale } }) {
+export default function RootLayout({ children }) {
   return (
-    <html lang={locale}>
+    <html lang="en">
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
-        <Header locale={locale} />
         <div className="flex-grow">
+          <Header />
           <div className="mx-auto">
             {children}
           </div>
         </div>
-        <Footer locale={locale} />
+        <Footer />
       </body>
     </html>
   );
-}
-
-export default appWithTranslation(RootLayout);
-
-export async function generateStaticParams() {
-  return [{ locale: 'en' }, { locale: 'ko' }];
 }
