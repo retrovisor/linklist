a1009170ab528ec8b6cd2a2b455666f67f83ed19
retrovisor <andrenfaust@gmail.com>
2024-06-06 15:22:37 +0200

    Update UsernameFormResult.js

--- src/components/formResults/UsernameFormResult.js
+++ src/components/formResults/UsernameFormResult.js
@@ -1,7 +1,7 @@
 export default function UsernameFormResult() {
   return (
     <div className="bg-red-200 border border-red-500 p-2 mb-2 text-center">
-      Taken username
+      사용자 이름이 사용되었습니다.
     </div>
   );
 }
