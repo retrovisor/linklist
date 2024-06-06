7100952f9bbbbdc8b4a539a90d4909b1ac63d3a5
retrovisor <andrenfaust@gmail.com>
2024-06-06 15:15:23 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -6,13 +6,13 @@ import '../globals.css';
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
 export const metadata = {
-  title: 'Fizz.link - Your one link for everything',
-  description: 'Share your links, social profiles, contact info and more on one page',
+  title: 'Fizz.link - 모든 것을 하나의 링크로 만드세요',
+  description: '여러분의 링크, 소셜 프로필, 창작물, 연락처 정보 등을 한 공간에서 공유하세요',
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="kr">
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <div className="flex-grow">
           <Header />
