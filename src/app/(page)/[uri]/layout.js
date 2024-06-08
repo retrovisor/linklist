f1765bdfd6d6068b690c5b3a01ff1668dd6f7ec1
retrovisor <andrenfaust@gmail.com>
2024-06-08 15:21:34 +0200

    Update layout.js

--- src/app/(page)/[uri]/layout.js
+++ src/app/(page)/[uri]/layout.js
@@ -11,15 +11,35 @@ export async function generateMetadata({ params }) {
     await mongoose.connect(process.env.MONGO_URI);
     const uri = params.uri;
     const page = await Page.findOne({ uri });
-
     if (!page) {
       return {
         title: '페이지를 찾을 수 없음 | Fizz.link',
       };
     }
-
     return {
       title: `${page.displayName} (@${page.uri}) | Fizz.link`,
+      openGraph: {
+        title: page.displayName,
+        description: page.bio,
+        url: `https://fizz.link/${page.uri}`,
+        type: 'website',
+        images: [
+          {
+            url: page.image,
+            width: 800,
+            height: 600,
+            alt: 'User image',
+          },
+        ],
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: page.displayName,
+        description: page.bio,
+        site: '@FizzLink',
+        creator: '@FizzLink',
+        images: [page.image],
+      },
     };
   } catch (error) {
     console.error('Error generating metadata:', error);
