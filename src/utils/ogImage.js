94bf27c9789f393c975453c347cec63fa8e66de6
retrovisor <andrenfaust@gmail.com>
2024-06-08 22:22:46 +0200

    Update ogImage.js

--- src/utils/ogImage.js
+++ src/utils/ogImage.js
@@ -1,31 +1,23 @@
-import sharp from 'sharp';
+import Jimp from 'jimp';
 import path from 'path';
 import fs from 'fs';
 
 export async function generateOgImage(backgroundImageUrl, avatarImageUrl) {
   try {
-    const backgroundImage = sharp(backgroundImageUrl);
-    const avatarImage = sharp(avatarImageUrl);
-
-    const backgroundMetadata = await backgroundImage.metadata();
-    const avatarMetadata = await avatarImage.metadata();
+    const backgroundImage = await Jimp.read(backgroundImageUrl);
+    const avatarImage = await Jimp.read(avatarImageUrl);
 
     const outputFilename = `og-image-${Date.now()}.png`;
     const outputImagePath = path.join(process.cwd(), 'public', 'og-images', outputFilename);
 
     const avatarSize = 200;
-    const avatarX = (backgroundMetadata.width - avatarSize) / 2;
-    const avatarY = (backgroundMetadata.height - avatarSize) / 2;
+    const avatarX = (backgroundImage.bitmap.width - avatarSize) / 2;
+    const avatarY = (backgroundImage.bitmap.height - avatarSize) / 2;
 
-    const generatedImage = await backgroundImage
-      .composite([
-        {
-          input: await avatarImage.resize(avatarSize, avatarSize).toBuffer(),
-          top: avatarY,
-          left: avatarX,
-        },
-      ])
-      .toFile(outputImagePath);
+    avatarImage.resize(avatarSize, avatarSize);
+    backgroundImage.composite(avatarImage, avatarX, avatarY);
+
+    await backgroundImage.writeAsync(outputImagePath);
 
     return `/og-images/${outputFilename}`;
   } catch (error) {
