74ae6929872a4a7a594bbcebd2b49fa60752480e
retrovisor <andrenfaust@gmail.com>
2024-06-12 15:46:02 +0200

    Update LoginWithKakao.js

--- src/components/buttons/LoginWithKakao.js
+++ src/components/buttons/LoginWithKakao.js
@@ -6,18 +6,11 @@ import kakaoLogo from '/public/kakao.png';
 import { useRouter } from "next/navigation";
 
 export default function LoginWithKakao() {
-  const router = useRouter();
-
-  const handleLogin = () => {
-    const desiredUsername = window.localStorage.getItem('desiredUsername');
-    signIn('kakao', {
-      callbackUrl: desiredUsername ? `/account?desiredUsername=${desiredUsername}` : '/account',
-    });
-  };
-
+  
   return (
     <button
-      onClick={handleLogin}
+            onClick={() => signIn('kakao')}
+
       className="bg-yellow-400 shadow text-center w-full py-4 flex gap-3 items-center justify-center"
     >
       <Image src={kakaoLogo} alt="Kakao" width={24} height={24} className="h-6" />
