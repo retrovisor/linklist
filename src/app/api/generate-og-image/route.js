7c2713770ff48ab4a4e7208065f6c44441e670b4
retrovisor <andrenfaust@gmail.com>
2024-06-14 14:02:11 +0200

    Update route.js

--- src/app/api/generate-og-image/route.js
+++ src/app/api/generate-og-image/route.js
@@ -1,19 +1,16 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import Jimp from 'jimp';
 import uniqid from 'uniqid';
-import { Page } from '@/models/Page';
-import mongoose from 'mongoose';
 import path from 'path';
 import clientPromise from "@/libs/mongoClient";
- 
 
 export async function POST(request) {
   const { backgroundImageUrl, avatarImageUrl, pageUri, bgColor } = await request.json();
-  try {
-    const timeoutId = setTimeout(() => {
-      throw new Error('Image generation timed out');
-    }, 60000);
+  const timeoutId = setTimeout(() => {
+    throw new Error('Image generation timed out');
+  }, 60000);
 
+  try {
     let background;
     if (backgroundImageUrl) {
       background = await Jimp.read(backgroundImageUrl);
@@ -78,15 +75,18 @@ export async function POST(request) {
     const customDomain = 'momofriends.com/naelink';
     const link = `https://${customDomain}/${newFilename}`;
 
-        const client = await clientPromise;
+    const client = await clientPromise;
     const db = client.db();
-    await db.collection("pages").findOneAndUpdate({ uri: pageUri }, { $set: { ogImageUrl: link } });
+    await db.collection("pages").findOneAndUpdate(
+      { uri: pageUri },
+      { $set: { ogImageUrl: link } }
+    );
 
-
-    clearTimeout(timeoutId);
     return new Response(JSON.stringify({ success: true, link }), { status: 200 });
   } catch (error) {
     console.error('Failed to generate OG image:', error);
     return new Response(JSON.stringify({ error: 'Internal Server Error', details: error.message }), { status: 500 });
+  } finally {
+    clearTimeout(timeoutId);
   }
 }
