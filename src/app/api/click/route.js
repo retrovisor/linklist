7ae752d0a588dcfe49acee04afd9c193bd44b0f0
retrovisor <andrenfaust@gmail.com>
2024-06-14 13:32:26 +0200

    Update route.js

--- src/app/api/click/route.js
+++ src/app/api/click/route.js
@@ -7,6 +7,6 @@ export async function POST(req) {
   const url = new URL(req.url);
   const clickedLink = atob(url.searchParams.get('url'));
   const page = url.searchParams.get('page');
-  await Event.create({type:'click', uri: clickedLink, page});
+  await db.collection("events").insertOne({type:'click', uri: clickedLink, page});
   return Response.json(true);
 }
