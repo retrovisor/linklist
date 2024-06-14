4f3d11f23fda52aa1ca788d7c30f99817de1ac22
retrovisor <andrenfaust@gmail.com>
2024-06-14 13:07:57 +0200

    Update mongoClient.js

--- src/libs/mongoClient.js
+++ src/libs/mongoClient.js
@@ -1,4 +1,5 @@
 import { MongoClient } from "mongodb";
+import retry from "async-retry";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -13,12 +14,32 @@ let clientPromise;
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    global._mongoClientPromise = retry(
+      async () => {
+        await client.connect();
+        return client;
+      },
+      {
+        retries: 3,
+        minTimeout: 1000,
+        maxTimeout: 5000,
+      }
+    );
   }
   clientPromise = global._mongoClientPromise;
 } else {
   client = new MongoClient(uri, options);
-  clientPromise = client.connect();
+  clientPromise = retry(
+    async () => {
+      await client.connect();
+      return client;
+    },
+    {
+      retries: 3,
+      minTimeout: 1000,
+      maxTimeout: 5000,
+    }
+  );
 }
 
 export default clientPromise;
