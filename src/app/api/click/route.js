b96a57e6499c5fb03fdf022052002203455ea679
retrovisor <andrenfaust@gmail.com>
2024-06-14 13:31:46 +0200

    Update route.js

--- src/app/api/click/route.js
+++ src/app/api/click/route.js
@@ -1,7 +1,9 @@
 import {Event} from '@/models/Event';
 import mongoose from "mongoose";
+import clientPromise from "@/libs/mongoClient";
+
 export async function POST(req) {
-  mongoose.connect(process.env.MONGO_URI);
+  const client = await clientPromise; const db = client.db();
   const url = new URL(req.url);
   const clickedLink = atob(url.searchParams.get('url'));
   const page = url.searchParams.get('page');
