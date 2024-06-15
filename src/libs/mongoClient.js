5c5939bfa7a42bc09aa1bb09810229a553a2ae9f
retrovisor <andrenfaust@gmail.com>
2024-06-15 16:11:12 +0200

    Update mongoClient.js

--- src/libs/mongoClient.js
+++ src/libs/mongoClient.js
@@ -1,5 +1,4 @@
 import { MongoClient } from "mongodb";
-import retry from "async-retry";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -10,54 +9,49 @@ const options = {
   maxPoolSize: 50,
   minPoolSize: 10,
   maxIdleTimeMS: 30000,
-  serverSelectionTimeoutMS: 15000, // Increase the server selection timeout to 15 seconds
+  serverSelectionTimeoutMS: 15000,
   socketTimeoutMS: 45000,
   connectTimeoutMS: 30000,
 };
+
 let client;
 let clientPromise;
 
+const connect = async () => {
+  try {
+    client = new MongoClient(uri, options);
+    await client.connect();
+    console.log("Connected to MongoDB");
+    return client;
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+};
+
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = retry(
-      async () => {
-        try {
-          await client.connect();
-          console.log("Connected to MongoDB");
-          return client;
-        } catch (error) {
-          console.error("Error connecting to MongoDB:", error);
-          throw error;
-        }
-      },
-      {
-        retries: 3,
-        minTimeout: 1000,
-        maxTimeout: 5000,
-      }
-    );
+    global._mongoClientPromise = connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, options);
-  clientPromise = retry(
-    async () => {
-      try {
-        await client.connect();
-        console.log("Connected to MongoDB");
-        return client;
-      } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error;
-      }
-    },
-    {
-      retries: 3,
-      minTimeout: 1000,
-      maxTimeout: 5000,
-    }
-  );
+  clientPromise = connect();
 }
+
+// Attempt to reconnect if there is an error or the connection is lost
+client.on("error", async (error) => {
+  console.error("MongoDB connection error:", error);
+  try {
+    await client.close();
+  } catch (closeError) {
+    console.error("Error closing MongoDB connection:", closeError);
+  }
+  clientPromise = connect();
+});
+
+client.on("close", async () => {
+  console.warn("MongoDB connection closed. Attempting to reconnect...");
+  clientPromise = connect();
+});
 
 export default clientPromise;
