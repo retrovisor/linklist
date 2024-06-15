ea71730bd60100e6656db72439649b857cd6e80a
retrovisor <andrenfaust@gmail.com>
2024-06-16 00:57:20 +0200

    Update grabUsername.js

--- src/actions/grabUsername.js
+++ src/actions/grabUsername.js
@@ -1,22 +1,28 @@
 'use server';
 import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 import {Page} from "@/models/Page";
-import mongoose from "mongoose";
 import {getServerSession} from "next-auth";
 import clientPromise from "@/libs/mongoClient";
 
-
 export default async function grabUsername(formData) {
   const username = formData.get('username');
-  const client = await clientPromise; const db = client.db();
-  const existingPageDoc = await db.collection("pages").findOne({uri:username});
-  if (existingPageDoc) {
-    return false;
-  } else {
-    const session = await getServerSession(authOptions);
-    return await db.collection("pages").insertOne({
-      uri:username,
-      owner:session?.user?.email,
-    });
+  
+  try {
+    const client = await clientPromise;
+    const db = client.db();
+
+    const existingPageDoc = await db.collection("pages").findOne({ uri: username });
+    if (existingPageDoc) {
+      return false;
+    } else {
+      const session = await getServerSession(authOptions);
+      return await db.collection("pages").insertOne({
+        uri: username,
+        owner: session?.user?.email,
+      });
+    }
+  } catch (error) {
+    console.error('Error in grabUsername:', error);
+    throw error;
   }
 }
