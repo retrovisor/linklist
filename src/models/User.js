502ca9ac2cb44e631db78766bef211090db99eaa
retrovisor <andrenfaust@gmail.com>
2024-06-16 00:43:39 +0200

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,14 @@
-import {model, models, Schema} from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 const UserSchema = new Schema({
   name: String,
   email: String,
   image: String,
   emailVerified: Date,
+  providerId: {
+    type: String,
+    unique: true, // Add a unique constraint to ensure providerId is unique across users
+  },
 });
 
 export const User = models?.User || model('User', UserSchema);
