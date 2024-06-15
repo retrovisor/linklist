8e8215243248ee7c16998cf4836acf8f9a0b79ed
retrovisor <andrenfaust@gmail.com>
2024-06-16 00:41:54 +0200

    Update route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -11,13 +11,21 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.sub, // Use the 'sub' field as the providerId for Google
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
     }),
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID,
       clientSecret: process.env.KAKAO_CLIENT_SECRET,
       profile(profile) {
         return {
-          id: profile.id,
+          id: profile.id, // Use the 'id' field as the providerId for Kakao
           name: profile.kakao_account.profile.nickname,
           email: profile.kakao_account.email,
           image: profile.kakao_account.profile.profile_image_url.replace('http://', 'https://'),
@@ -33,14 +41,16 @@ export const authOptions = {
     encryption: true,
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user, account }) {
       if (user) {
         token.id = user.id;
+        token.providerId = account.provider + ':' + user.id; // Add this line to include the providerId
       }
       return token;
     },
     async session({ session, token }) {
       session.user.id = token.id;
+      session.user.providerId = token.providerId; // Add this line to include the providerId in the session
       
       // Check if the user's image URL exists, otherwise set the default image URL
       if (!session.user.image) {
