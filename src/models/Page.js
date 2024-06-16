eece25811c4c8b5713191a590029c58a24ee4e3f
retrovisor <andrenfaust@gmail.com>
2024-06-16 16:47:20 +0200

    Update Page.js

--- src/models/Page.js
+++ src/models/Page.js
@@ -2,13 +2,7 @@ import { model, models, Schema } from "mongoose";
 
 const PageSchema = new Schema({
   uri: { type: String, required: true, unique: true, minlength: 1 },
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    get: (v) => v.toString(),
-    set: (v) => new ObjectId(v)
-  }, 
+  owner: { type: Schema.Types.ObjectId, ref: 'User', required: true }, // Change the owner field to reference the User model by _id
   displayName: { type: String, default: '' },
   location: { type: String, default: '' },
   bio: { type: String, default: '' },
