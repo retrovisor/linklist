6804ea9866bf7fa5d41d0a7596eb10fb4c0f8ece
retrovisor <andrenfaust@gmail.com>
2024-06-16 23:41:11 +0200

    Update LoginWithGoogle.js

--- src/components/buttons/LoginWithGoogle.js
+++ src/components/buttons/LoginWithGoogle.js
@@ -4,9 +4,14 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {signIn} from "next-auth/react";
 
 export default function LoginWithGoogle() {
+  const handleClick = () => {
+    console.log('Clicked on LoginWithGoogle button');
+    signIn('google');
+  };
+
   return (
     <button
-      onClick={() => signIn('google')}
+      onClick={handleClick}
       className="bg-white shadow text-center w-full py-4 flex gap-3 items-center justify-center">
       <FontAwesomeIcon icon={faGoogle} className="h-6" />
       <span>구글로 로그인</span>
