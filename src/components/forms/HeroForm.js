178c9dafab21d6854c6d6d2dd22123ac668ff6c9
retrovisor <andrenfaust@gmail.com>
2024-06-16 23:38:54 +0200

    Update HeroForm.js

--- src/components/forms/HeroForm.js
+++ src/components/forms/HeroForm.js
@@ -1,5 +1,4 @@
-'use client';
-
+// 'use client';
 import { signIn } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
@@ -23,29 +22,30 @@ export default function HeroForm({ user }) {
     const form = ev.target;
     const input = form.querySelector('input');
     const username = input.value;
+
+    console.log('Desired username (HeroForm):', username); // Log the desired username
+
     if (username.length > 0) {
       if (user) {
         router.push('/account?desiredUsername=' + username);
       } else {
         window.localStorage.setItem('desiredUsername', username);
         router.push('/login?desiredUsername=' + username);
-
       }
     }
   }
 
-    return (
+  return (
     <div className="w-full max-w-lg mx-auto">
       <form
         onSubmit={handleSubmit}
         className="flex flex-col sm:flex-row items-stretch sm:space-x-4 space-y-4 sm:space-y-0"
       >
         <input
-  type="text"
-  className="!bg-white border rounded-md flex-1 !py-4 !px-4 sm:py-4 sm:px-6 !w-auto !mb-0 !p-0"
-  placeholder="사용자 이름 입력"
-/>
-
+          type="text"
+          className="!bg-white border rounded-md flex-1 !py-4 !px-4 sm:py-4 sm:px-6 !w-auto !mb-0 !p-0"
+          placeholder="사용자 이름 입력"
+        />
         <button
           type="submit"
           className="btn-link3 text-white whitespace-nowrap !py-4 !px-4 sm:py-4 sm:px-6"
@@ -56,4 +56,3 @@ export default function HeroForm({ user }) {
     </div>
   );
 }
-
