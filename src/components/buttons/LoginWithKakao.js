ea596ab3c1b5a9ec0003d813b77f553478d7f61d
retrovisor <andrenfaust@gmail.com>
2024-06-16 23:42:01 +0200

    Update LoginWithKakao.js

--- src/components/buttons/LoginWithKakao.js
+++ src/components/buttons/LoginWithKakao.js
@@ -5,11 +5,14 @@ import Image from "next/image";
 import kakaoLogo from '/public/kakao.png';
  
 export default function LoginWithKakao() {
+  const handleClick = () => {
+    console.log('Clicked on LoginWithKakao button');
+    signIn('kakao');
+  };
   
   return (
     <button
-            onClick={() => signIn('kakao')}
-
+      onClick={handleClick}
       className="bg-yellow-400 shadow text-center w-full py-4 flex gap-3 items-center justify-center"
     >
       <Image src={kakaoLogo} alt="Kakao" width={24} height={24} className="h-6" />
@@ -17,3 +20,4 @@ export default function LoginWithKakao() {
     </button>
   );
 }
+
