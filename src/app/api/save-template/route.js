e9e26c53693b62a5be4216aad711ae4393f96880
retrovisor <andrenfaust@gmail.com>
2024-06-18 01:20:45 +0200

    Update route.js

--- src/app/api/save-template/route.js
+++ src/app/api/save-template/route.js
@@ -2,31 +2,26 @@ import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { Page } from '@/models/Page';
-import mongoose from 'mongoose';
 import clientPromise from "@/libs/mongoClient";
 
 
 export async function POST(request) {
-  const client = await clientPromise; const db = client.db();
-
+  const client = await clientPromise;
+  const db = client.db();
   const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.redirect('/api/auth/signin');
   }
-
   const { template } = await request.json();
-
   try {
-    const page = await db.collection("pages").findOneAndUpdate(
+    const page = await db.collection('pages').findOneAndUpdate(
       { owner: session.user.id },
       { $set: { template } },
-      { new: true }
+      { returnOriginal: false }
     );
-
-    if (!page) {
+    if (!page.value) {
       return NextResponse.json({ success: false, message: 'Page not found' });
     }
-
     return NextResponse.json({ success: true });
   } catch (error) {
     return NextResponse.json({ success: false, error: error.message });
