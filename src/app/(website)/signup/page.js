eacf4d8eb97e0ed022b86801be2d4444867eb83f
retrovisor <andrenfaust@gmail.com>
2024-06-19 15:14:34 +0200

    Update page.js

--- src/app/(website)/signup/page.js
+++ src/app/(website)/signup/page.js
@@ -1,9 +1,8 @@
 // app/(website)/login/page.js
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
-import LoginWithKakao from "@/components/buttons/LoginWithKakao";
-import LoginWithFacebook from "@/components/buttons/LoginWithFacebook";
+import LoginWithGoogle from "@/components/buttons/SignupWithGoogle";
+import LoginWithKakao from "@/components/buttons/SignupWithKakao";
 import UsernameForm from "@/components/forms/UsernameForm";
 import { redirect } from "next/navigation";
 import clientPromise from "@/libs/mongoClient";
@@ -29,13 +28,11 @@ export default async function LoginPage({ searchParams }) {
           <p className="text-center mb-6 text-gray-500">
             아래 방법을 사용하여 무료로 Fizz.link 계정을 생성하세요
           </p>
-          <LoginWithKakao />
+          <SignupWithKakao />
           <div className="mt-4">
-            <LoginWithGoogle />
+            <SignupWithGoogle />
           </div>
-          <div className="mt-4">
-            <LoginWithFacebook />
-          </div>
+          
         </div>
       </div>
     );
