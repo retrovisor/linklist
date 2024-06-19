ffdd20d8d0fb490c4f0f4a9ce352475052508b5e
retrovisor <andrenfaust@gmail.com>
2024-06-19 13:50:34 +0200

    Create Fathom.js

---
+++ src/components/Fathom.js
@@ -0,0 +1,32 @@
+// Fathom.js
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import * as Fathom from 'fathom-client';
+
+function TrackPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    Fathom.load('FIEGHTLD', {
+      auto: false,
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!pathname) return;
+
+    Fathom.trackPageview({
+      url: pathname + searchParams.toString(),
+      referrer: document.referrer,
+    });
+  }, [pathname, searchParams]);
+
+  return null;
+}
+
+export default function Fathom() {
+  return <TrackPageView />;
+}
