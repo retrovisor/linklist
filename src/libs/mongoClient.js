b8d3e162ea6a44d6881d24576afff70d6fe4f1a3
retrovisor <andrenfaust@gmail.com>
2024-06-21 12:49:17 +0200

    Update mongoClient.js

--- src/libs/mongoClient.js
+++ src/libs/mongoClient.js
@@ -8,28 +8,24 @@ const uri = process.env.MONGODB_URI;
 let client;
 let clientPromise;
 
+const options = {
+  maxPoolSize: 10,
+  minPoolSize: 5,
+  retryWrites: true,
+  w: "majority",
+};
+
 if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, {
-      maxPoolSize: 10,
-      minPoolSize: 5,
-      retryWrites: true,
-      w: "majority",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
-  client = new MongoClient(uri, {
-    maxPoolSize: 10,
-    minPoolSize: 5,
-    retryWrites: true,
-    w: "majority",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
