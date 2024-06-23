3417f9b27523e2cfb361b680f4f47a985c8baa62
retrovisor <andrenfaust@gmail.com>
2024-06-24 00:06:54 +0200

    Update page.js

--- src/app/(website)/contact/page.js
+++ src/app/(website)/contact/page.js
@@ -1,18 +1,23 @@
+import { getDictionary } from '@/libs/getDictionary';
 import Link from 'next/link';
 
-export default function AboutPage() {
+export default async function ContactPage({ params: { lang } }) {
+  const dict = await getDictionary(lang);
+
   return (
     <div className="bg-blue-950 text-white min-h-screen">
       <div className="h-36 bg-gray-400 bg-cover bg-center">
-        <h1 className="text-4xl text-center pt-12">Contact Page</h1>
+        <h1 className="text-4xl text-center pt-12">{dict.contact.title}</h1>
       </div>
       <div className="max-w-xs mx-auto text-center my-4">
-        <p>Welcome to the contact page. Here you will find information about our website and mission.</p>
+        <p>{dict.contact.content}</p>
         {/* Add more static content as needed */}
       </div>
       <div className="max-w-2xl mx-auto grid md:grid-cols-2 gap-6 p-4 px-8">
-        <Link href="/">
-          <a className="bg-indigo-800 p-2 block flex items-center justify-center text-white">Go back to Home</a>
+        <Link href={`/${lang}`}>
+          <a className="bg-indigo-800 p-2 block flex items-center justify-center text-white">
+            {dict.common.backToHome}
+          </a>
         </Link>
       </div>
     </div>
