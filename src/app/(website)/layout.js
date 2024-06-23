b142db6d0324ccfe1ee1d042401c1d8d785793da
retrovisor <andrenfaust@gmail.com>
2024-06-23 23:37:38 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -3,27 +3,42 @@ import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
+import { useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export const metadata = {
-  title: 'Fizz.link - 모든 것을 하나의 링크로 만드세요',
-  description: '여러분의 링크, 소셜 프로필, 창작물, 연락처 정보 등을 한 공간에서 공유하세요',
-};
+export async function generateMetadata({ params: { lang } }) {
+  const { t } = await serverSideTranslations(lang, ['common']);
+  return {
+    title: t('metadata.title'),
+    description: t('metadata.description'),
+  };
+}
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children, params: { lang } }) {
+  const { t } = await serverSideTranslations(lang, ['common']);
+
   return (
-    <html lang="kr">
+    <html lang={lang}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-          <Header />
+          <Header t={t} />
           <div className="mx-auto">
             {children}
           </div>
         </div>
-        <Footer />
+        <Footer t={t} />
       </body>
     </html>
   );
+}
+
+export async function getStaticProps({ params: { lang } }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(lang, ['common'])),
+    },
+  };
 }
