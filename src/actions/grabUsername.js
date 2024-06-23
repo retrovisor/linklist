558e70a971d53d6d38541f79c5179e632ec9884e
retrovisor <andrenfaust@gmail.com>
2024-06-23 23:14:58 +0200

    Update grabUsername.js

--- src/actions/grabUsername.js
+++ src/actions/grabUsername.js
@@ -1,8 +1,7 @@
 'use server';
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
-import clientPromise from "@/libs/mongoClient";
-import { ObjectId } from 'mongodb';
+import { performDatabaseOperation } from "@/libs/mongoClient";
 
 function koreanToRomanized(text) {
   const initialConsonants = ['g', 'kk', 'n', 'd', 'tt', 'r', 'm', 'b', 'pp', 's', 'ss', 'ng', 'j', 'jj', 'ch', 'k', 't', 'p', 'h'];
@@ -51,17 +50,16 @@ export default async function grabUsername(formData) {
   }
 
   try {
-    const client = await clientPromise;
-    const db = client.db();
+    return await performDatabaseOperation(async (db) => {
+      // Check if a page with the same uri already exists
+      const existingPageDoc = await db.collection("pages").findOne({ uri: romanizedUsername });
+      if (existingPageDoc) {
+        console.log('Username already taken:', username); // Log if the username is already taken
+        return false;
+      }
 
-    // Check if a page with the same uri already exists
-    const existingPageDoc = await db.collection("pages").findOne({ uri: romanizedUsername });
-    if (existingPageDoc) {
-      console.log('Username already taken:', username); // Log if the username is already taken
-      return false;
-    } else {
       // Get the session to retrieve user information
-      const session = await getServerSession(authOptions);
+      const session = await getServerSession({ req: formData, res: null }, authOptions);
       if (!session || !session.user || !session.user.id) {
         throw new Error('User session not found');
       }
@@ -90,9 +88,10 @@ export default async function grabUsername(formData) {
 
       console.log('Username grabbed successfully:', romanizedUsername); // Log successful username grab
       return result.insertedId;
-    }
+    });
   } catch (error) {
     console.error('Error in grabUsername:', error);
     throw error;
   }
 }
+
