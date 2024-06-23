e992167b41900c15ea607f5a86bc0e4faaabaf64
retrovisor <andrenfaust@gmail.com>
2024-06-24 00:32:35 +0200

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,45 +1,28 @@
 import { NextResponse } from 'next/server';
-import { match as matchLocale } from '@formatjs/intl-localematcher';
-import Negotiator from 'negotiator';
 
-const LOCALES = ['en', 'kr'];
-const DEFAULT_LOCALE = 'en';
-
-function getLocale(request) {
-  const negotiatorHeaders = {};
-  request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-
-  const languages = new Negotiator({ headers: negotiatorHeaders }).languages();
-  return matchLocale(languages, LOCALES, DEFAULT_LOCALE);
-}
+const PUBLIC_FILE = /\.(.*)$/;
 
 export function middleware(request) {
-  const pathname = request.nextUrl.pathname;
-  const pathnameIsMissingLocale = LOCALES.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-  );
+  const { pathname } = request.nextUrl;
 
-  // Check if the pathname is for a user profile
-  const isUserProfile = pathname.split('/').length === 2 && pathname !== '/favicon.ico';
-
-  if (isUserProfile) {
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/static') ||
+    PUBLIC_FILE.test(pathname)
+  ) {
     return NextResponse.next();
   }
 
-  // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request);
-    return NextResponse.redirect(
-      new URL(`/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`, request.url)
-    );
+  if (pathname === '/') {
+    return NextResponse.redirect(new URL('/en', request.url));
   }
-}
 
-export const config = {
-  matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
-    // Optional: only run on root (/) URL
-    // '/'
-  ],
-};
+  const pathnameHasLocale = pathname.startsWith('/en') || pathname.startsWith('/kr');
+
+  if (!pathnameHasLocale) {
+    return NextResponse.redirect(new URL(`/en${pathname}`, request.url));
+  }
+
+  return NextResponse.next();
+}
