3e8bcbc560aed03fcd72f13bdf7bad975849136d
retrovisor <andrenfaust@gmail.com>
2024-06-23 23:52:55 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,31 +4,33 @@ import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import { getDictionary } from '@/lib/getDictionary';
 
-export default async function Header() {
+export default async function Header({ lang }) {
   const session = await getServerSession(authOptions);
+  const dict = await getDictionary(lang);
+
   return (
     <header className="py-4 sticky top-0 z-50 fundo-home">
       <div className="max-w-4xl flex justify-between mx-auto px-4 text-xl">
         <div className="flex items-center gap-6">
-          <Link href={'/'} className="flex items-center gap-1 text-blue-500">
-          <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
-
+          <Link href={`/${lang}`} className="flex items-center gap-1 text-blue-500">
+            <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
             <span className="font-bold cor-roxa">Fizz.link</span>
           </Link>
         </div>
         <nav className="flex items-center text-sm text-slate-500">
           {session ? (
             <>
-              <Link href={'/account'} className="btn-link2 mr-2">
-                내 페이지
+              <Link href={`/${lang}/account`} className="btn-link2 mr-2">
+                {dict.header.myPage}
               </Link>
-              <LogoutButton />
+              <LogoutButton label={dict.header.logout} />
             </>
           ) : (
             <>
-              <Link href={'/login'} className="btn-link2">로그인</Link>
-              <Link href={'/signup'} className="btn-link">무료 등록</Link>
+              <Link href={`/${lang}/login`} className="btn-link2">{dict.header.login}</Link>
+              <Link href={`/${lang}/signup`} className="btn-link">{dict.header.signup}</Link>
             </>
           )}
         </nav>
