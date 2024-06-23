cdc7c9f5f85074db908f5bca054345be2f1ba04a
retrovisor <andrenfaust@gmail.com>
2024-06-24 00:30:56 +0200

    Update getDictionary.js

--- src/libs/getDictionary.js
+++ src/libs/getDictionary.js
@@ -5,4 +5,10 @@ const dictionaries = {
   kr: () => import('@/dictionaries/kr.json').then((module) => module.default),
 }
 
-export const getDictionary = async (locale) => dictionaries[locale]()
+export const getDictionary = async (locale) => {
+  if (!dictionaries[locale]) {
+    console.warn(`Dictionary for locale "${locale}" not found. Falling back to English.`);
+    return dictionaries.en();
+  }
+  return dictionaries[locale]();
+}
