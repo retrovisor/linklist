c322013858dd5ecd3f12623e209857cfd6f8944d
retrovisor <andrenfaust@gmail.com>
2024-06-23 20:20:48 +0200

    Update route.js

--- src/app/api/save-template/route.js
+++ src/app/api/save-template/route.js
@@ -1,29 +1,32 @@
 import { NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { Page } from '@/models/Page';
-import clientPromise from "@/libs/mongoClient";
-
+import { performDatabaseOperation } from "@/libs/mongoClient";
 
 export async function POST(request) {
-  const client = await clientPromise;
-  const db = client.db();
   const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.redirect('/api/auth/signin');
   }
+
   const { template } = await request.json();
+
   try {
-    const page = await db.collection('pages').findOneAndUpdate(
-      { owner: session.user.id },
-      { $set: { template } },
-      { returnOriginal: false }
-    );
-    if (!page.value) {
+    const result = await performDatabaseOperation(async (db) => {
+      const page = await db.collection('pages').findOneAndUpdate(
+        { owner: session.user.id },
+        { $set: { template } },
+        { returnOriginal: false }
+      );
+      return page;
+    });
+
+    if (!result.value) {
       return NextResponse.json({ success: false, message: 'Page not found' });
     }
     return NextResponse.json({ success: true });
   } catch (error) {
+    console.error('Error saving template:', error);
     return NextResponse.json({ success: false, error: error.message });
   }
 }
