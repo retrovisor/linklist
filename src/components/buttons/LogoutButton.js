a323dd05a932e56d62b0d52790daf5920ea2e1cb
retrovisor <andrenfaust@gmail.com>
2024-06-23 23:59:14 +0200

    Update LogoutButton.js

--- src/components/buttons/LogoutButton.js
+++ src/components/buttons/LogoutButton.js
@@ -1,10 +1,10 @@
 'use client';
-import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {signOut} from "next-auth/react";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signOut } from "next-auth/react";
 
-export default function LogoutButton({ label }) {
-
+export default function LogoutButton({
+  label,
   className = 'flex items-center gap-2 border p-2 px-4 logout',
   iconLeft = false,
   iconClasses = '',
@@ -16,7 +16,7 @@ export default function LogoutButton({ label }) {
       {iconLeft && (
         <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
       )}
-      <span> {label} </span>
+      <span>{label}</span>
       {!iconLeft && (
         <FontAwesomeIcon icon={faRightFromBracket} className={iconClasses} />
       )}
