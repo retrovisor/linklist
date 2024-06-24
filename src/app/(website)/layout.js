86a25592edd2b2b45512d20de07e3d9463c2eb07
retrovisor <andrenfaust@gmail.com>
2024-06-24 17:52:42 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,14 +1,13 @@
-import { getDictionary } from '@/libs/getDictionary';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
+import { getDictionary } from '@/libs/getDictionary';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export async function generateMetadata({ searchParams }) {
-  const lang = searchParams?.lang || 'en';
+export async function generateMetadata({ params: { lang } }) {
   const dict = await getDictionary(lang);
   return {
     title: dict.metadata.title,
@@ -16,8 +15,8 @@ export async function generateMetadata({ searchParams }) {
   };
 }
 
-export default async function RootLayout({ children, searchParams }) {
-  const lang = searchParams?.lang || 'en';
+export default async function RootLayout({ children, params }) {
+  const lang = params?.lang || 'en';
   const dict = await getDictionary(lang);
 
   return (
