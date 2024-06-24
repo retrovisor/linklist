aca72da264fe7b2c8f0ea0854d8656e05f067cbe
retrovisor <andrenfaust@gmail.com>
2024-06-24 17:47:14 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,15 @@ import Link from "next/link";
 export default async function Header({ dict, lang }) {
   const session = await getServerSession(authOptions);
 
+  const addLangToHref = (href) => {
+    return `${href}${href.includes('?') ? '&' : '?'}lang=${lang}`;
+  };
+
   return (
     <header className="py-4 sticky top-0 z-50 fundo-home">
       <div className="max-w-4xl flex justify-between mx-auto px-4 text-xl">
         <div className="flex items-center gap-6">
-          <Link href={`/${lang}`} className="flex items-center gap-1 text-blue-500">
+          <Link href={addLangToHref('/')} className="flex items-center gap-1 text-blue-500">
             <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
             <span className="font-bold cor-roxa">Fizz.link</span>
           </Link>
@@ -18,15 +22,15 @@ export default async function Header({ dict, lang }) {
         <nav className="flex items-center text-sm text-slate-500">
           {session ? (
             <>
-              <Link href={`/${lang}/account`} className="btn-link2 mr-2">
+              <Link href={addLangToHref('/account')} className="btn-link2 mr-2">
                 {dict.header.myPage}
               </Link>
               <LogoutButton label={dict.header.logout} />
             </>
           ) : (
             <>
-              <Link href={`/${lang}/login`} className="btn-link2">{dict.header.login}</Link>
-              <Link href={`/${lang}/signup`} className="btn-link">{dict.header.signup}</Link>
+              <Link href={addLangToHref('/login')} className="btn-link2">{dict.header.login}</Link>
+              <Link href={addLangToHref('/signup')} className="btn-link">{dict.header.signup}</Link>
             </>
           )}
         </nav>
