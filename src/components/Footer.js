e9ff1bf07cab6e00d5210d64303363bdf36c5129
retrovisor <andrenfaust@gmail.com>
2024-06-24 17:55:29 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+// src/components/Footer.js
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LogoutButton from "@/components/buttons/LogoutButton";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
@@ -5,23 +6,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-export default async function Footer({ dict, lang }) {
+export default async function Footer({ locale }) {
   const session = await getServerSession(authOptions);
-
-  const addLangToHref = (href) => {
-    return `${href}${href.includes('?') ? '&' : '?'}lang=${lang}`;
-  };
 
   return (
     <footer className="fundo-home border-t py-6">
       <div className="max-w-4xl flex justify-between mx-auto px-4">
         <div className="flex items-center gap-6">
-          <Link href={addLangToHref('/')} className="flex items-center gap-1 text-blue-500">
+          <Link href={`/`} className="flex items-center gap-1 text-blue-500">
             <img src="/logo4.png" alt="Logo" style={{ width: '1em' }} />
             <span className="font-bold cor-roxa">Fizz.link</span>
           </Link>
           <nav className="flex items-center gap-4 text-slate-500 text-sm">
-            <Link href={addLangToHref('/about')}>{dict.footer.about}</Link>
+            <Link href={`/about`}>에 대한</Link>
           </nav>
         </div>
       </div>
