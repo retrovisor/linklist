d9ca2e11d28fc1b4565b1fc9ee08c73e5b8344be
retrovisor <andrenfaust@gmail.com>
2024-06-25 12:06:33 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,27 +1,17 @@
+// layout.js
+import { getServerSession } from 'next-auth/next';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { getDictionary } from '@/libs/getDictionary';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export async function generateMetadata({ params }) {
-  const lang = params?.lang || 'en';
-  const dict = await getDictionary(lang);
-  return {
-    title: dict.metadata.title,
-    description: dict.metadata.description,
-  };
-}
-
 export default async function RootLayout({ children, searchParams }) {
   const lang = searchParams?.lang || 'en';
-  const dict = await getDictionary(lang);
   const session = await getServerSession(authOptions);
 
   console.log('RootLayout searchParams:', searchParams);
@@ -33,25 +23,7 @@ export default async function RootLayout({ children, searchParams }) {
     console.log('RootLayout dict:', dict);
   } catch (error) {
     console.error('Error loading dictionary:', error);
-    return (
-      <div className="bg-white text-black min-h-screen p-4">
-        <h1 className="text-2xl font-bold">Error Loading Page</h1>
-        <p>An error occurred while loading the page content. Please try again later.</p>
-        <pre className="mt-4 p-2 bg-gray-100 rounded">
-          {JSON.stringify(error, null, 2)}
-        </pre>
-      </div>
-    );
-  }
-
-  if (!dict) {
-    console.error('Dictionary is undefined:', dict);
-    return (
-      <div className="bg-white text-black min-h-screen p-4">
-        <h1 className="text-2xl font-bold">Page Content Unavailable</h1>
-        <p>We&apos;re sorry, but the page content could not be loaded at this time.</p>
-      </div>
-    );
+    // Handle the error appropriately
   }
 
   return (
@@ -59,9 +31,9 @@ export default async function RootLayout({ children, searchParams }) {
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-        <Header dict={dict} session={session} />
+          <Header dict={dict} session={session} />
           <div className="mx-auto">
-            {children}
+            {React.cloneElement(children, { lang })}
           </div>
         </div>
         <Footer dict={dict} lang={lang} />
