fc066bb3980043352d779ae1c4cf9322e31b1f08
retrovisor <andrenfaust@gmail.com>
2024-06-25 15:41:41 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,4 +1,3 @@
-// layout.js
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { getDictionary } from '@/libs/getDictionary';
@@ -8,21 +7,14 @@ import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
 import React from 'react';
+import { getSearchParams } from '@/libs/getSearchParams';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export async function generateMetadata({ searchParams }) {
+export async function getServerSideProps(context) {
+  const searchParams = getSearchParams(context.req);
   const lang = searchParams?.lang || 'en';
-  const dict = await getDictionary(lang);
-  return {
-    title: dict.metadata.title,
-    description: dict.metadata.description,
-  };
-}
-
-export default async function RootLayout({ children, searchParams }) {
-  const lang = searchParams?.lang || 'en';
-  const session = await getServerSession(authOptions);
+  const session = await getServerSession(context.req, context.res, authOptions);
 
   console.log('RootLayout searchParams:', searchParams);
   console.log('RootLayout lang:', lang);
@@ -36,12 +28,31 @@ export default async function RootLayout({ children, searchParams }) {
     // Handle the error appropriately
   }
 
+  return {
+    props: {
+      lang,
+      dict,
+      session
+    },
+  };
+}
+
+export async function generateMetadata({ searchParams }) {
+  const lang = searchParams?.lang || 'en';
+  const dict = await getDictionary(lang);
+  return {
+    title: dict.metadata.title,
+    description: dict.metadata.description,
+  };
+}
+
+function RootLayout({ children, lang, dict, session }) {
   return (
     <html lang={lang}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-          <Header dict={dict} lang={lang} />
+          <Header dict={dict} lang={lang} session={session} />
           <div className="mx-auto">
             {React.cloneElement(children, { lang })}
           </div>
@@ -51,3 +62,5 @@ export default async function RootLayout({ children, searchParams }) {
     </html>
   );
 }
+
+export default RootLayout;
