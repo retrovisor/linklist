a6b5cc64a8e8ca0319566657cc2b78984ec2bd43
retrovisor <andrenfaust@gmail.com>
2024-06-25 16:26:27 +0200

    Update getSearchParams.js

--- src/libs/getSearchParams.js
+++ src/libs/getSearchParams.js
@@ -1,10 +1,25 @@
 // src/libs/getSearchParams.js
 export function getSearchParams(segment) {
-  if (!segment) return {};
+  console.log('getSearchParams called with segment:', segment);
+
+  if (!segment) {
+    console.log('Empty segment, returning empty object');
+    return {};
+  }
+
   const searchParams = {};
-  segment.split('?')[1]?.split('&').forEach((param) => {
-    const [key, value] = param.split('=');
-    searchParams[key] = value;
-  });
+
+  const paramsString = segment.split('?')[1];
+  console.log('Extracted params string:', paramsString);
+
+  if (paramsString) {
+    paramsString.split('&').forEach((param) => {
+      const [key, value] = param.split('=');
+      searchParams[key] = value;
+      console.log(`Parsed param: ${key} = ${value}`);
+    });
+  }
+
+  console.log('Returning searchParams:', searchParams);
   return searchParams;
 }
