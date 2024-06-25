6d5fc3b16fbff3729c06f2f9b65c6437395776eb
retrovisor <andrenfaust@gmail.com>
2024-06-25 12:52:09 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -11,21 +11,34 @@ import React from 'react';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export default async function RootLayout({ children }) {
+// layout.js
+export default async function RootLayout({ children, params }) {
+  const lang = params?.lang || 'en';
   const session = await getServerSession(authOptions);
-  const dict = await getDictionary('en');
+
+  console.log('RootLayout params:', params);
+  console.log('RootLayout lang:', lang);
+
+  let dict;
+  try {
+    dict = await getDictionary(lang);
+    console.log('RootLayout dict:', dict);
+  } catch (error) {
+    console.error('Error loading dictionary:', error);
+    // Handle the error appropriately
+  }
 
   return (
-    <html lang="en">
+    <html lang={lang}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-          <Header dict={dict} />
+          <Header dict={dict} lang={lang} />
           <div className="mx-auto">
-            {children}
+            {React.cloneElement(children, { lang })}
           </div>
         </div>
-        <Footer dict={dict} />
+        <Footer dict={dict} lang={lang} />
       </body>
     </html>
   );
