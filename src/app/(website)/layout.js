42bb458b88c92544cd69d8229b82ce1846af7eb0
retrovisor <andrenfaust@gmail.com>
2024-06-25 12:28:35 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -2,18 +2,18 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { getDictionary } from '@/libs/getDictionary';
- import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
 import React from 'react';
-import { useSearchParams } from 'next/navigation';
+
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-export default async function RootLayout({ children }) {
-  const searchParams = useSearchParams();
-  const lang = searchParams.get('lang') || 'en';
+export default async function RootLayout({ children, searchParams }) {
+  const lang = searchParams?.lang || 'en';
   const session = await getServerSession(authOptions);
 
   console.log('RootLayout searchParams:', searchParams);
@@ -33,7 +33,8 @@ export default async function RootLayout({ children }) {
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-           <div className="mx-auto">
+          <Header dict={dict} session={session} />
+          <div className="mx-auto">
             {React.cloneElement(children, { lang })}
           </div>
         </div>
