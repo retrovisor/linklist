92c8d64ac2248555beca1acc75b25463e73eb951
retrovisor <andrenfaust@gmail.com>
2024-06-25 12:24:03 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -2,8 +2,7 @@
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { getDictionary } from '@/libs/getDictionary';
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+ import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
@@ -34,8 +33,7 @@ export default async function RootLayout({ children }) {
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-          <Header dict={dict} session={session} />
-          <div className="mx-auto">
+           <div className="mx-auto">
             {React.cloneElement(children, { lang })}
           </div>
         </div>
