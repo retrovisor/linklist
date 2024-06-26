4b05afe8bd2864fd47000aa4ca5d191fed858c6a
retrovisor <andrenfaust@gmail.com>
2024-06-26 14:25:32 +0200

    Update page.js

--- src/app/(localized)/l/[lang]/about/page.js
+++ src/app/(localized)/l/[lang]/about/page.js
@@ -1,11 +1,20 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import AboutPageClient from './AboutPageClient';
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['about'])),
+    },
+  };
+}
 
 export default async function AboutPage({ params: { lang } }) {
   console.log('AboutPage function started');
   try {
-    // Any server-side logic can go here if needed
+    const translations = await serverSideTranslations(lang, ['about']);
     
-    return <AboutPageClient lang={lang} />;
+    return <AboutPageClient lang={lang} translations={translations} />;
   } catch (error) {
     console.error('Error in AboutPage:', error);
     return <div>An error occurred</div>;
