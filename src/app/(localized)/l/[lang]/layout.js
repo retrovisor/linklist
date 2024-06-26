fd49afd79813bd03f3485a32aa34025f20acc16e
retrovisor <andrenfaust@gmail.com>
2024-06-26 14:51:21 +0200

    Update layout.js

--- src/app/(localized)/l/[lang]/layout.js
+++ src/app/(localized)/l/[lang]/layout.js
@@ -1,17 +1,15 @@
+// src/app/(localized)/l/[lang]/layout.js
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../../../globals.css';
 import TrackPageView from "@/components/Fathom";
 import { dir } from 'i18next';
-import { appWithTranslation } from 'next-i18next';
-
+import I18nextProvider from '../../i18next-provider'; // Adjust the import path as necessary
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
 export async function generateMetadata({ params: { lang } }) {
-  // You might need to implement a server-side translation function here
-  // or use a different method to get translations for metadata
   return {
     title: 'Fizz.link - 모든 것을 하나의 링크로 만드세요',
     description: '여러분의 링크, 소셜 프로필, 창작물, 연락처 정보 등을 한 공간에서 공유하세요',
@@ -23,13 +21,15 @@ export default function LocalizedLayout({ children, params: { lang } }) {
     <html lang={lang} dir={dir(lang)}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
-        <div className="flex-grow">
-          <Header />
-          <div className="mx-auto">
-            {children}
+        <I18nextProvider>
+          <div className="flex-grow">
+            <Header />
+            <div className="mx-auto">
+              {children}
+            </div>
           </div>
-        </div>
-        <Footer />
+          <Footer />
+        </I18nextProvider>
       </body>
     </html>
   );
