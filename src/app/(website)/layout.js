9b461321fb2089f40a625840f48210297c594230
retrovisor <andrenfaust@gmail.com>
2024-06-26 12:11:24 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,40 +1,29 @@
-// src/app/(website)/layout.js
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { getDictionary } from '@/libs/getDictionary';
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
-import React from 'react';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
-async function RootLayout({ children, headers }) {
-  const session = await getServerSession(authOptions);
-  const searchParams = new URLSearchParams(headers().get('query'));
-  const lang = searchParams.get('lang') || 'en';
-  const dict = await getDictionary(lang);
+export const metadata = {
+  title: 'Fizz.link - 모든 것을 하나의 링크로 만드세요',
+  description: '여러분의 링크, 소셜 프로필, 창작물, 연락처 정보 등을 한 공간에서 공유하세요',
+};
 
-  console.log('RootLayout searchParams:', Object.fromEntries(searchParams.entries()));
-  console.log('RootLayout lang:', lang);
-  console.log('RootLayout dict:', dict);
-
+export default function RootLayout({ children }) {
   return (
-    <html lang={lang}>
+    <html lang="kr">
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
-          <Header dict={dict} lang={lang} session={session} />
+          <Header />
           <div className="mx-auto">
-            {React.cloneElement(children, { lang })}
+            {children}
           </div>
         </div>
-        <Footer dict={dict} lang={lang} />
+        <Footer />
       </body>
     </html>
   );
 }
-
-export default RootLayout;
