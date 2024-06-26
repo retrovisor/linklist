8b3912dff878efbffd057a4d70955f350084c1de
retrovisor <andrenfaust@gmail.com>
2024-06-26 12:16:03 +0200

    Update layout.js

--- src/app/(website)/layout.js
+++ src/app/(website)/layout.js
@@ -1,8 +1,11 @@
+// app/(localized)/layout.js
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
+import { useTranslation } from 'next-i18next';
+import { dir } from 'i18next';
 
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -11,9 +14,11 @@ export const metadata = {
   description: '여러분의 링크, 소셜 프로필, 창작물, 연락처 정보 등을 한 공간에서 공유하세요',
 };
 
-export default function RootLayout({ children }) {
+export default function LocalizedLayout({ children, params: { lang } }) {
+  const { t } = useTranslation('common');
+
   return (
-    <html lang="kr">
+    <html lang={lang} dir={dir(lang)}>
       <body className={`${lato.className} min-h-screen fundo-home flex flex-col`}>
         <TrackPageView />
         <div className="flex-grow">
