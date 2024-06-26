3423ea31ac08dd8eed5e71e6a07a71ad6cf9d93f
retrovisor <andrenfaust@gmail.com>
2024-06-26 15:28:45 +0200

    Update page.js

--- src/app/(localized)/l/[lang]/about/page.js
+++ src/app/(localized)/l/[lang]/about/page.js
@@ -1,21 +1,11 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import AboutPageClient from './AboutPageClient';
 
-export async function generateMetadata({ params: { lang } }) {
-  return {
-    title: 'Fizz.link - 모든 것을 하나의 링크로 만드세요',
-    description: '여러분의 링크, 소셜 프로필, 창작물, 연락처 정보 등을 한 공간에서 공유하세요',
-  };
-}
-
 export default async function AboutPage({ params: { lang } }) {
-  console.log('AboutPage function started with lang:', lang);
+  console.log('AboutPage function started');
   try {
-    // Fetch translations on the server side
-    const translations = await serverSideTranslations(lang, ['about']);
-    console.log('Translations fetched:', translations);
-
-    return <AboutPageClient lang={lang} translations={translations} />;
+    // Any server-side logic can go here if needed
+    
+    return <AboutPageClient lang={lang} />;
   } catch (error) {
     console.error('Error in AboutPage:', error);
     return <div>An error occurred</div>;
