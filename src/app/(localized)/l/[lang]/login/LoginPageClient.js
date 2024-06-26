ee329ef5107ca0e52b01767d1b28261fcc9209f7
retrovisor <andrenfaust@gmail.com>
2024-06-26 17:36:21 +0200

    Update LoginPageClient.js

--- src/app/(localized)/l/[lang]/login/LoginPageClient.js
+++ src/app/(localized)/l/[lang]/login/LoginPageClient.js
@@ -1,13 +1,35 @@
 'use client';
 
-import { useTranslation } from 'next-i18next';
+import { useEffect } from 'react';
+import { useTranslation, initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
 import LoginWithGoogle from "@/components/buttons/LoginWithGoogle";
 import LoginWithKakao from "@/components/buttons/LoginWithKakao";
 import LoginWithFacebook from "@/components/buttons/LoginWithFacebook";
 import UsernameForm from "@/components/forms/UsernameForm";
 
-export default function LoginPageClient({ session, searchParams }) {
-  const { t } = useTranslation('login');
+export default function LoginPageClient({ session, searchParams, translations }) {
+  useEffect(() => {
+    console.log('LoginPageClient mounted with translations:', translations);
+
+    i18n.use(initReactI18next).init({
+      resources: {
+        [translations.lang]: {
+          translation: translations,
+        },
+      },
+      lng: translations.lang,
+      fallbackLng: 'en',
+      interpolation: {
+        escapeValue: false,
+      },
+      react: {
+        useSuspense: false,
+      },
+    });
+  }, [translations]);
+
+  const { t } = useTranslation();
 
   if (session) {
     console.log('User is logged in, rendering UsernameForm');
