7c7a5cc90f4ced695c897f101a7d911a26dd3034
retrovisor <andrenfaust@gmail.com>
2024-06-26 16:09:57 +0200

    Update [lang].js

--- src/app/api/translations/[lang].js
+++ src/app/api/translations/[lang].js
@@ -6,6 +6,6 @@ export async function GET(req, { params }) {
     const translations = await serverSideTranslations(lang, ['about']);
     return new Response(JSON.stringify(translations), { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify({ error: 'Failed to fetch translations' }), { status: 500 });
+    return new Response(JSON.stringify({ error: 'Failed to fetch translations2' }), { status: 500 });
   }
 }
