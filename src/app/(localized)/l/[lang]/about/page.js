c4c10ad38a62e45f3f620689d7f46af5e613e0db
retrovisor <andrenfaust@gmail.com>
2024-06-26 15:37:16 +0200

    Update page.js

--- src/app/(localized)/l/[lang]/about/page.js
+++ src/app/(localized)/l/[lang]/about/page.js
@@ -1,11 +1,17 @@
 import AboutPageClient from './AboutPageClient';
+import { getTranslations } from '../../../../../../libs/getTranslations';
+
+export async function generateStaticParams() {
+  return [{ lang: 'en' }, { lang: 'kr' }];
+}
 
 export default async function AboutPage({ params: { lang } }) {
-  console.log('AboutPage function started');
+  console.log('AboutPage function started with lang:', lang);
   try {
-    // Any server-side logic can go here if needed
-    
-    return <AboutPageClient lang={lang} />;
+    const translations = await getTranslations(lang, ['about']);
+    console.log('Translations fetched:', translations);
+
+    return <AboutPageClient lang={lang} translations={translations} />;
   } catch (error) {
     console.error('Error in AboutPage:', error);
     return <div>An error occurred</div>;
