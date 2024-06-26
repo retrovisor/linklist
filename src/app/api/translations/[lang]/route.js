46a5ec14eb68d05e09b59b6d8dfe2ec94303f4ab
retrovisor <andrenfaust@gmail.com>
2024-06-26 16:51:30 +0200

    Update and rename src/app/api/translations/[lang].js to src/app/api/translations/[lang]/route.js

--- src/app/api/translations/[lang]/route.js
+++ src/app/api/translations/[lang]/route.js
@@ -1,16 +1,25 @@
 'use client';
-
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-export async function GET(req, { params }) {
+export async function GET(request, { params }) {
   const { lang } = params;
   console.log(`API route called for lang: ${lang}`);
   try {
     const translations = await serverSideTranslations(lang, ['about']);
     console.log('Translations in API route:', translations);
-    return new Response(JSON.stringify(translations), { status: 200 });
+    return new Response(JSON.stringify(translations), { 
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   } catch (error) {
     console.error('Error fetching translations in API route:', error);
-    return new Response(JSON.stringify({ error: 'Failed to fetch translations' }), { status: 500 });
+    return new Response(JSON.stringify({ error: 'Failed to fetch translations' }), { 
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
