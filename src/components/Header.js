130557855418ab24bfc9216f8ee23b0a11d0666d
retrovisor <andrenfaust@gmail.com>
2024-06-26 12:05:55 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,34 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import LogoutButton from "@/components/buttons/LogoutButton";
-import { getServerSession } from "next-auth/next";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 
-export default async function Header({ dict = {}, lang, session }) {
-
-  console.log('Header lang:', lang);
-  console.log('Header dict:', dict);
-
-  const addLangToHref = (href) => {
-    return `${href}${href.includes('?') ? '&' : '?'}lang=${lang}`;
-  };
-
+export default async function Header() {
+  const session = await getServerSession(authOptions);
   return (
     <header className="py-4 sticky top-0 z-50 fundo-home">
       <div className="max-w-4xl flex justify-between mx-auto px-4 text-xl">
         <div className="flex items-center gap-6">
-          <Link href={addLangToHref('/')} className="flex items-center gap-1 text-blue-500">
-            <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
+          <Link href={'/'} className="flex items-center gap-1 text-blue-500">
+          <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
+
             <span className="font-bold cor-roxa">Fizz.link</span>
           </Link>
         </div>
         <nav className="flex items-center text-sm text-slate-500">
           {session ? (
             <>
-              <Link href={addLangToHref('/account')} className="btn-link2 mr-2">
-                {dict.header?.myPage || 'My Page'}
+              <Link href={'/account'} className="btn-link2 mr-2">
+                내 페이지
               </Link>
-              <LogoutButton label={dict.header?.logout || 'Logout'} />
+              <LogoutButton />
             </>
           ) : (
             <>
-              <Link href={addLangToHref('/login')} className="btn-link2">{dict.header?.login || 'Login'}</Link>
-              <Link href={addLangToHref('/signup')} className="btn-link">{dict.header?.signup || 'Sign Up'}</Link>
+              <Link href={'/login'} className="btn-link2">로그인</Link>
+              <Link href={'/signup'} className="btn-link">무료 등록</Link>
             </>
           )}
         </nav>
