40ecbd63c3494542f67e4fb0431167b277dbfa61
retrovisor <andrenfaust@gmail.com>
2024-06-26 13:51:48 +0200

    Create settings.js

---
+++ src/app/i18n/settings.js
@@ -0,0 +1,16 @@
+// app/i18n/settings.js
+export const fallbackLng = 'en';
+export const languages = [fallbackLng, 'kr'];
+export const defaultNS = 'translation';
+
+export function getOptions (lng = fallbackLng, ns = defaultNS) {
+  return {
+    // debug: true,
+    supportedLngs: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns
+  };
+}
