42af99bed0295f995a498f35f5c27730d2245fe6
retrovisor <andrenfaust@gmail.com>
2024-06-26 16:21:43 +0200

    Update [lang].js

--- src/app/api/translations/[lang].js
+++ src/app/api/translations/[lang].js
@@ -2,10 +2,13 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 export async function GET(req, { params }) {
   const { lang } = params;
+  console.log(`API route called for lang: ${lang}`);
   try {
     const translations = await serverSideTranslations(lang, ['about']);
+    console.log('Translations in API route:', translations);
     return new Response(JSON.stringify(translations), { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify({ error: 'Failed to fetch translations2' }), { status: 500 });
+    console.error('Error fetching translations in API route:', error);
+    return new Response(JSON.stringify({ error: 'Failed to fetch translations' }), { status: 500 });
   }
 }
