d94b67017daecad3ad7ae5a6edcc86683fe13ffc
retrovisor <andrenfaust@gmail.com>
2024-06-26 17:49:03 +0200

    Update SignupPageClient.js

--- src/app/(localized)/l/[lang]/signup/SignupPageClient.js
+++ src/app/(localized)/l/[lang]/signup/SignupPageClient.js
@@ -1,12 +1,48 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import i18n from 'i18next';
+import { useTranslation, initReactI18next } from 'react-i18next';
 import SignupWithGoogle from "@/components/buttons/SignupWithGoogle";
 import SignupWithKakao from "@/components/buttons/SignupWithKakao";
 import UsernameForm from "@/components/forms/UsernameForm";
-import { useTranslation } from 'next-i18next';
 
-export default function SignupPageClient({ session, searchParams, lang }) {
-  const { t } = useTranslation('signup');
+export default function SignupPageClient({ session, searchParams, translations }) {
+  const [i18nInitialized, setI18nInitialized] = useState(false);
+
+  useEffect(() => {
+    console.log('SignupPageClient mounted with translations:', translations);
+
+    i18n.use(initReactI18next).init({
+      resources: {
+        [translations.lang]: {
+          translation: translations,
+        },
+      },
+      lng: translations.lang,
+      fallbackLng: 'en',
+      interpolation: {
+        escapeValue: false,
+      },
+      react: {
+        useSuspense: false,
+      },
+    }).then(() => {
+      setI18nInitialized(true);  // Update state to trigger re-render
+    });
+  }, [translations]);
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (i18nInitialized) {
+      console.log('t function loaded, checking translation keys:');
+      console.log('createAccount:', t('createAccount'));
+      console.log('createAccountDescription:', t('createAccountDescription'));
+    }
+  }, [i18nInitialized, t]);
+
+  if (!i18nInitialized) return null; // Optionally render a loading state
 
   if (session) {
     console.log('User is logged in, rendering UsernameForm');
@@ -14,7 +50,7 @@ export default function SignupPageClient({ session, searchParams, lang }) {
     return <UsernameForm desiredUsername={desiredUsername} />;
   }
 
-  console.log('User is not logged in, rendering login buttons');
+  console.log('User is not logged in, rendering signup buttons');
   return (
     <div>
       <div className="p-4 max-w-xs mx-auto">
