f08037716e33e0e16a900b7e8f01ae06451fd3d5
retrovisor <andrenfaust@gmail.com>
2024-06-30 10:14:35 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,22 @@
+// components/Header.js
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import LogoutButton from "@/components/buttons/LogoutButton";
-import { faLink } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { getServerSession } from "next-auth";
 import Link from "next/link";
+import HeaderNav from './HeaderNav';
 
 export default async function Header() {
   const session = await getServerSession(authOptions);
+
   return (
     <header className="py-4 sticky top-0 z-50 fundo-home">
       <div className="max-w-4xl flex justify-between mx-auto px-4 text-xl">
         <div className="flex items-center gap-6">
           <Link href={'/'} className="flex items-center gap-1 text-blue-500">
-          <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
-
+            <img src="/logo4.png" alt="Logo" style={{ width: '1.2em' }} />
             <span className="font-bold cor-roxa">Fizz.link</span>
           </Link>
         </div>
-        <nav className="flex items-center text-sm text-slate-500">
-          {session ? (
-            <>
-              <Link href={'/account'} className="btn-link2 mr-2">
-                내 페이지
-              </Link>
-              <LogoutButton />
-            </>
-          ) : (
-            <>
-              <Link href={'/login'} className="btn-link2">로그인</Link>
-              <Link href={'/signup'} className="btn-link">무료 등록</Link>
-            </>
-          )}
-        </nav>
+        <HeaderNav session={session} />
       </div>
     </header>
   );
