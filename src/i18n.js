5f31874c009dbcdc977cbca40a7f9d85ff8f3f86
retrovisor <andrenfaust@gmail.com>
2024-06-30 10:38:25 +0200

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,27 +1,20 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import HttpBackend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
 import nextI18NextConfig from '../next-i18next.config';
 
 console.log('Initializing i18n with config:', nextI18NextConfig);
 
 i18n
-  .use(HttpBackend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     ...nextI18NextConfig.i18n,
     fallbackLng: 'en',
     debug: process.env.NODE_ENV === 'development',
     interpolation: {
-      escapeValue: false,
+      escapeValue: false, // not needed for react as it escapes by default
     },
     react: {
       useSuspense: false,
-    },
-    backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
   }).then(() => {
     console.log('i18n initialized');
