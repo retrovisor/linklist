a784ce0566c613bf4c7046662927b236eb81f8ab
retrovisor <andrenfaust@gmail.com>
2024-06-30 10:54:35 +0200

    Update ClientI18nextProvider.js

--- src/app/(localized)/l/[lang]/ClientI18nextProvider.js
+++ src/app/(localized)/l/[lang]/ClientI18nextProvider.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../../../i18n'; // Adjust the path as necessary
+import i18n from '../../../../i18n'; // Adjust the path as necessary
 import { useEffect } from 'react';
 
 export default function ClientI18nextProvider({ children, lang }) {
