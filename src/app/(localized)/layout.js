eea48c289761fc0c9f48644baf50af90d65fc84d
retrovisor <andrenfaust@gmail.com>
2024-06-30 10:39:06 +0200

    Update layout.js

--- src/app/(localized)/layout.js
+++ src/app/(localized)/layout.js
@@ -1,5 +1,4 @@
 import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { Lato } from 'next/font/google';
 import '../globals.css';
 import TrackPageView from "@/components/Fathom";
